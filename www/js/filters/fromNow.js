834a088e5826ecc443f512c400b4a7f73416df42
Kashif Zaidi <kashifzaidi@hotmail.com>
2015-12-02 17:59:25 +0500

    updated filters to handle null or zero values

--- www/js/filters/fromNow.js
+++ www/js/filters/fromNow.js
@@ -2,7 +2,9 @@ angular.module('starter')
 	// returns human readable age i.e 10 minutes ago
 	.filter('fromNow', function(){
 	    return function(value){
-	        var d = new Date(value * 1000);
-	        return moment(d).fromNow();
+	    	if(value){
+	    		var d = new Date(value * 1000);
+	    		return moment(d).fromNow();
+	    	} else return "";
 	    }
 	});
