5d09d4437bc5c68a2a262fe451e490d32172daa1
Kashif Zaidi <kashifzaidi@hotmail.com>
2015-12-02 17:06:18 +0500

    removed mashape

--- chromeApps/mindfirst/private_configs/mindfirst.config.js
+++ chromeApps/mindfirst/private_configs/mindfirst.config.js
@@ -5,11 +5,5 @@ window.private_keys = {
     "client_secrets": {
         "Chrome": "mindfirst_chrome_app_secret"
     },
-    "mashape_keys": {
-        "Web": {
-            "Testing": "wsLZ3OhaZLmshirpn5Z50QWfTHF4p1bCTfCjsnPtPjzdfenrJr",
-            "Production": "BzQoPobn35mshnCpyddJ7TYYXfwSp1Yjhj8jsnAOadHk0qMbVU"
-        }
-    },
     "bugsnag_key":'601992ccea9ea09c8a2e750b4427a04b'
 };
