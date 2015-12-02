2b6f95ec4ffeb1929d66e7c174e7ef1650965592
Kashif Zaidi <kashifzaidi@hotmail.com>
2015-12-02 13:57:39 +0500

    updated import

--- platforms/ios/www/js/controllers/importCtrl.js
+++ platforms/ios/www/js/controllers/importCtrl.js
@@ -35,7 +35,16 @@ angular.module('starter')
 
 	            $ionicLoading.hide();
 
-	            window.qmSetupOnIonic();
+	            if(ionic.Platform.platforms[0] === "browser"){
+	            	window.qmSetupOnIonic();
+	            } else {	            	
+	            	var targetUrl = config.getURL("api/v1/connect/mobile", true);
+	            	targetUrl += "access_token="+token.accessToken;
+	            	var ref = window.open(targetUrl,'_blank', 'location=no,toolbar=yes');
+	            	ref.addEventListener('exit', function(){
+						$state.go('app.track');
+					});
+	            }	            
 
 	        }, function(){
 
