a2fac4dee57c86b5ead602209945b86a3d364599
Kashif Zaidi <kashifzaidi@hotmail.com>
2016-01-03 21:32:22 +0500

    travis upload

--- scripts/package-and-upload.sh
+++ scripts/package-and-upload.sh
@@ -6,6 +6,7 @@
 # Make the ipa file #
 #####################
 - cd platforms/ios
-- xcodebuild -workspace MoodiModo.xcworkspace -scheme MoodiModo -sdk iphoneos -configuration AppStoreDistribution archive -archivePath $PWD/build/MoodiModo.xcarchive
-- xcodebuild -exportArchive -archivePath $PWD/build/MoodiModo.xcarchive -exportPath $PWD/build
+- xcodebuild -workspace MoodiModo.xcworkspace -scheme MoodiModo -sdk iphoneos -configuration Release  archive -archivePath $PWD/build/MoodiModo.xcarchive
+- xcodebuild -exportArchive -archivePath $PWD/build/MoodiModo.xcarchive -exportPath $PWD/build -exportOptionsPlist $PWD/exportOptions.plist
+# - pilot upload -u ios@quantimodo.com -i $PWD/build/MoodiModo.ipa -a com.quantimodo.moodimodoapp --verbose
 
