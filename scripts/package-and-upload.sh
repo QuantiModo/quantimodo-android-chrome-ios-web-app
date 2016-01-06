7a6fdaeb89d13f99b5d67d4a775d9f2f162a411b
Kashif Zaidi <kashifzaidi@hotmail.com>
2016-01-06 18:00:50 +0500

    updated packageing

--- scripts/package-and-upload.sh
+++ scripts/package-and-upload.sh
@@ -1,12 +1,8 @@
 
 #!/bin/sh
 
-
-#####################
-# Make the ipa file #
-#####################
-- cd platforms/ios
-- xcodebuild -workspace MoodiModo.xcworkspace -scheme MoodiModo -sdk iphoneos -configuration Release  archive -archivePath $PWD/build/MoodiModo.xcarchive
-- xcodebuild -exportArchive -archivePath $PWD/build/MoodiModo.xcarchive -exportPath $PWD/build -exportOptionsPlist $PWD/exportOptions.plist
-# - pilot upload -u ios@quantimodo.com -i $PWD/build/MoodiModo.ipa -a com.quantimodo.moodimodoapp --verbose
+cd platforms/ios
+xcodebuild -workspace MoodiModo.xcworkspace -scheme MoodiModo -sdk iphoneos -configuration Release  archive -archivePath $PWD/build/MoodiModo.xcarchive
+xcodebuild -exportArchive -archivePath $PWD/build/MoodiModo.xcarchive -exportPath $PWD/build -exportOptionsPlist $PWD/exportOptions.plist
+pilot upload -u ios@quantimodo.com -i $PWD/build/MoodiModo.ipa -a com.quantimodo.moodimodoapp --verbose
 
