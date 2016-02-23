b399de0f25595ccdf3c74a6518109f1072addcb5
Kashif Zaidi <kashifzaidi@hotmail.com>
2016-02-23 10:39:25 +0500

    fix for podfile lock

--- scripts/package-and-upload.sh
+++ scripts/package-and-upload.sh
@@ -5,6 +5,15 @@ cd platforms/ios
 
 xcodebuild -workspace MoodiModo.xcworkspace -scheme MoodiModo -sdk iphoneos -configuration Release  archive -archivePath $PWD/build/MoodiModo.xcarchive CODE_SIGN_IDENTITY="$DEVELOPER_NAME" PROVISIONING_PROFILE="$PROFILE_UUID" CODE_SIGN_IDENTITY[sdk=iphoneos*]="$DEVELOPER_NAME"
 
+cd ../../
+
+rm -rf MoodiModo.xcworkspace
+pod install
+
+cd platforms/ios
+
+xcodebuild -workspace MoodiModo.xcworkspace -scheme MoodiModo -sdk iphoneos -configuration Release  archive -archivePath $PWD/build/MoodiModo.xcarchive CODE_SIGN_IDENTITY="$DEVELOPER_NAME" PROVISIONING_PROFILE="$PROFILE_UUID" CODE_SIGN_IDENTITY[sdk=iphoneos*]="$DEVELOPER_NAME"
+
 xcodebuild -exportArchive -archivePath $PWD/build/MoodiModo.xcarchive -exportPath $PWD/build -exportOptionsPlist $PWD/exportOptions.plist
 
 pilot upload -u ios@quantimodo.com -i $PWD/build/MoodiModo.ipa -a com.quantimodo.moodimodoapp -p 1046797567 --verbose
