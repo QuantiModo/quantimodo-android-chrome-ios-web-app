77e4e3c2b0147229379ee36362a9d21fee449b93
Kashif Zaidi <kashifzaidi@hotmail.com>
2016-02-25 19:15:28 +0500

    removed the project directory

--- platforms/ios/www/js/filters/time.js
+++
@@ -1,9 +0,0 @@
-angular.module('starter')
-	// returns string for time
-	.filter('time', function(){
-	    return function(time){
-	    	if(time){
-	    		return moment(time*1000).format("MMM Do YYYY, hh:mm a").split(/,/g);;
-	    	} else return "";
-	    }
-	});
