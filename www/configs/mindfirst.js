2013d5dc3e012863f9ff613ffeb0aff0f3fd9f47
Mike P. Sinn <m@quantimodo.com>
2016-03-20 18:58:47 -0500

    Added Settings back to menus

--- www/configs/mindfirst.js
+++ www/configs/mindfirst.js
@@ -372,6 +372,11 @@ config.appSettings  = {
             icon : 'ion-sad-outline'
         },
         {
+            title : 'Settings',
+            href : '#/app/settings',
+            icon : 'ion-ios-gear-outline'
+        },
+        {
             title : 'Help & Feedback',
             href : window.chrome ? "mailto:help@quantimo.do" : "#app/feedback",
             icon : 'ion-ios-help-outline'
