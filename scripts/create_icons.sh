ca290dc52c0333be028ea2dcd1015b0ca61fab20
Mike P. Sinn <m@quantimodo.com>
2016-03-25 12:28:59 -0500

    Copy large icon

--- scripts/create_icons.sh
+++ scripts/create_icons.sh
@@ -7,6 +7,7 @@ if ! type "imagemagick" > /dev/null;
 fi
 
 ionic resources
+cp resources/icon.png www/img/icon_700.png
 convert resources/icon.png -resize 16x16 www/img/icon_16.png
 convert resources/icon.png -resize 48x48 www/img/icon_48.png
 convert resources/icon.png -resize 128x128 www/img/icon_128.png
