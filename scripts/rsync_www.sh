a30e6a5799069739a3ac1dea8d73ab44d58f1169
Mike P. Sinn <m@quantimodo.com>
2016-03-26 16:00:28 -0500

    Added rsync script

---
+++ scripts/rsync_www.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT=/vagrant
+IONIC_PATH="${PROJECT_ROOT}/public.built/ionic/Modo"
+CHROME_EXTENSION_PATH="${IONIC_PATH}/resources/chrome_extension"
+
+APP_PATH=${IONIC_PATH}/resources/chrome_app
+APP_CONFIG=${PROJECT_ROOT}/configs/chrome_apps/${APP_NAME}.config.js
+
+# rsync -a /vagrant/public.built/ionic/Modo/www/ /vagrant/public.built/ionic/Modo/resources/chrome_extension/www
+
+while :
+do
+    now=$(date +"%T")
+    echo "Current time : $now"
+    rsync -a /vagrant/public.built/ionic/Modo/www/ /vagrant/public.built/ionic/Modo/resources/chrome_extension/www
+    echo "Synced www to resources/chrome_extension/www. Sleeping 5 seconds."
+    sleep 5
+done
