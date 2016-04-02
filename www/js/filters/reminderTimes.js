57583586d9308fddc176f5b595a3d05b7a64ef44
Mike P. Sinn <m@quantimodo.com>
2016-04-01 19:33:57 -0500

    Display reminder times in local timezone

--- www/js/filters/reminderTimes.js
+++ www/js/filters/reminderTimes.js
@@ -5,19 +5,31 @@ angular.module('starter')
         
 	    return function(reminder){
 	    	
-	    	var parseDate = function(reminderTime){	    		
+	    	var parseDate = function(reminderTime){
+
+				var now = new Date();
+				var hourOffsetFromUtc = now.getTimezoneOffset()/60;
+				console.log('Hours from UTC is ' + hourOffsetFromUtc);
+
 	    		var parsedDate = reminderTime.split(':');
-	    		return moment().hours(parsedDate[0]).minutes(parsedDate[1]);
+				var minutes = parsedDate[1];
+				var localHour = parsedDate[0] + hourOffsetFromUtc;
+				if(localHour > 23){
+					localHour = localHour - 24;
+				}
+	    		return moment().hours(localHour).minutes(minutes);
 	    	};
 
+
+
 	    	if (reminder.firstDailyReminderTime && reminder.secondDailyReminderTime && reminder.thirdDailyReminderTime){
-	    		return  "at " + parseDate(reminder.firstDailyReminderTime).format("hh:mm A") + ", " +
+	    		return  reminder.frequencyTextDescription + " at " + parseDate(reminder.firstDailyReminderTime).format("hh:mm A") + ", " +
 	    		parseDate(reminder.secondDailyReminderTime).format("hh:mm A")+ " and " +
 	    		parseDate(reminder.thirdDailyReminderTime).format("hh:mm A");
 	    	}
 
 			if (reminder.firstDailyReminderTime && reminder.secondDailyReminderTime){
-	    		return "at " + parseDate(reminder.firstDailyReminderTime).format('hh:mm A') + " and " + 
+	    		return reminder.frequencyTextDescription + " at " + parseDate(reminder.firstDailyReminderTime).format('hh:mm A') + " and " +
 	    		parseDate(reminder.secondDailyReminderTime).format("hh:mm A");
 	    	}
 
@@ -25,6 +37,6 @@ angular.module('starter')
 	    		return "at " + parseDate(reminder.firstDailyReminderTime).format("hh:mm A");
 	    	}
 
-			return "";
+			return reminder.frequencyTextDescription;
 	    };
 	});
