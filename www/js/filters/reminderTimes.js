978a54e353bb1fe6f5d4ef6c09156ba5917f7f5b
Mike P. Sinn <m@quantimodo.com>
2016-04-02 18:37:09 -0500

    I had my use of getTimezoneOffset backwards

--- www/js/filters/reminderTimes.js
+++ www/js/filters/reminderTimes.js
@@ -11,12 +11,15 @@ angular.module('starter')
 				var hourOffsetFromUtc = now.getTimezoneOffset()/60;
 	    		var parsedDate = reminderTime.split(':');
 				var minutes = parsedDate[1];
-				var localHour = parseInt(parsedDate[0]) + parseInt(hourOffsetFromUtc);
 				
+				var localHour = parseInt(parsedDate[0]) - parseInt(hourOffsetFromUtc);
 				if(localHour > 23){
 					localHour = localHour - 24;
 				}
-	    		
+                if(localHour < 0){
+                    localHour = localHour + 24;
+                }
+				
 	    		return moment().hours(localHour).minutes(minutes);
 	    	};
 
