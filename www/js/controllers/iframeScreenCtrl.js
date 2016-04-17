038113c25278493dc2a490a6f9808a4009abf38c
Mike P. Sinn <m@quantimodo.com>
2016-04-17 17:34:07 -0500

    Set page title dynamically in iframe template

--- www/js/controllers/iframeScreenCtrl.js
+++ www/js/controllers/iframeScreenCtrl.js
@@ -13,18 +13,21 @@ angular.module('starter')
         if(window.location.href.indexOf('search-variables') > -1 )
         {
             embedPlugin = 'search-variables';
+            $scope.title = 'Your Variables';
         }
 
         if(window.location.href.indexOf('search-common-relationships') > -1 )
         {
             embedPlugin = 'search-relationships';
             urlParameters = '&commonOrUser=common';
+            $scope.title = 'Common Variable Relationships';
         }
 
         if(window.location.href.indexOf('search-user-relationships') > -1 )
         {
             embedPlugin = 'search-relationships';
             urlParameters = '&commonOrUser=user';
+            $scope.title = 'Your Variable Relationships';
         }
 
         authService.getAccessTokenFromAnySource().then(function(token) {
