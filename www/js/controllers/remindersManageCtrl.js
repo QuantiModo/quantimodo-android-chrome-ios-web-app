764fdbeecf07f3c8562c86aa6497da0cb485def9
Mike P. Sinn <m@quantimodo.com>
2016-04-23 19:53:02 -0500

    Removed redundant filterViaDates and getTrackingReminderNotficiations from remindersManageCtrl

--- www/js/controllers/remindersManageCtrl.js
+++ www/js/controllers/remindersManageCtrl.js
@@ -51,60 +51,6 @@ angular.module('starter')
 
 		};
 
-	    var filterViaDates = function(reminders){
-
-	    	var result = [];
-	    	var reference = moment().local();
-	    	var today = reference.clone().startOf('day');
-	    	var yesterday = reference.clone().subtract(1, 'days').startOf('day');
-	    	var weekold = reference.clone().subtract(7, 'days').startOf('day');
-	    	var monthold = reference.clone().subtract(30, 'days').startOf('day');
-
-	    	var todayResult = reminders.filter(function(reminder){
-	    		return moment.utc(reminder.trackingReminderNotificationTime).local().isSame(today, 'd') === true;
-	    	});
-
-	    	if(todayResult.length) {
-				result.push({name: "Today", reminders: todayResult});
-            }
-
-	    	var yesterdayResult = reminders.filter(function(reminder){
-	    		return moment.utc(reminder.trackingReminderNotificationTime).local().isSame(yesterday, 'd') === true;
-	    	});
-
-	    	if(yesterdayResult.length) result.push({ name : "Yesterday", reminders : yesterdayResult });
-
-	    	var last7DayResult = reminders.filter(function(reminder){
-	    		var date = moment.utc(reminder.trackingReminderNotificationTime).local();
-
-	    		return date.isAfter(weekold) === true
-	    		&& date.isSame(yesterday, 'd') !== true
-	    		&& date.isSame(today, 'd') !== true;
-	    	});
-
-	    	if(last7DayResult.length) result.push({ name : "Last 7 Days", reminders : last7DayResult });
-
-	    	var last30DayResult = reminders.filter(function(reminder){
-
-	    		var date = moment.utc(reminder.trackingReminderNotificationTime).local();
-
-	    		return date.isAfter(monthold) === true
-	    		&& date.isBefore(weekold) === true
-	    		&& date.isSame(yesterday, 'd') !== true
-	    		&& date.isSame(today, 'd') !== true;
-	    	});
-
-	    	if(last30DayResult.length) result.push({ name : "Last 30 Days", reminders : last30DayResult });
-
-	    	var olderResult = reminders.filter(function(reminder){
-	    		return moment.utc(reminder.trackingReminderNotificationTime).local().isBefore(monthold) === true;
-	    	});
-
-	    	if(olderResult.length) result.push({ name : "Older", reminders : olderResult });
-
-	    	return result;
-	    };
-
 	    var utils = {
     	    startLoading : function(){
     	    	// show spinner
@@ -154,23 +100,7 @@ angular.module('starter')
 				utilsService.showLoginRequiredAlert($scope.login);
 	    	});
 	    };
-
-	    var getTrackingReminderNotifications = function(){
-	    	utils.startLoading();
-
-	    	reminderService.getTrackingReminderNotifications($stateParams.variableCategoryName)
-	    	.then(function(reminders){
-	    		$scope.state.trackingRemindersNotifications = reminders;
-	    		$scope.state.filteredReminders = filterViaDates(reminders);
-	    		utils.stopLoading();
-	    	}, function(){
-	    		utils.stopLoading();
-	    		console.log("failed to get reminder notifications");
-				//utilsService.showLoginRequiredAlert($scope.login);
-
-	    	});
-	    };
-
+		
 	    $scope.cancel = function(){
 	    	$scope.state.showMeasurementBox = !$scope.state.showMeasurementBox;
 	    	
