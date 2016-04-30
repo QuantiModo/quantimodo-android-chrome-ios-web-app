90785bcd138ed34713db3b2a6e9eb6b5702355b0
Mike P. Sinn <m@quantimodo.com>
2016-04-30 18:48:27 -0500

    Reverted change of default app

--- www/js/apps.js
+++ www/js/apps.js
@@ -5,7 +5,7 @@ var appsManager = {
 		"mindfirst" : "configs/mindfirst",
 		"medtlc" : "configs/medtlc"
 	},
-	defaultApp : "medtlc",
+	defaultApp : "moodimodo",
 	getDefaultConfig : function(){
 		return appsManager.apps[appsManager.defaultApp] ? appsManager.apps[appsManager.defaultApp]+'.js' : false;
 	},
