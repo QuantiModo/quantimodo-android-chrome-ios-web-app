208c0a6950488c74e297862de025889a58b0f094
Mike P. Sinn <m@quantimodo.com>
2016-05-10 12:31:09 -0500

    ionic resources >/dev/null

--- scripts/create_icons.sh
+++ scripts/create_icons.sh
@@ -11,7 +11,7 @@
 
 ionic platform add ios
 ionic platform add android
-ionic resources
+ionic resources >/dev/null
 cp resources/icon.png www/img/icon_700.png
 convert resources/icon.png -resize 16x16 www/img/icon_16.png
 convert resources/icon.png -resize 48x48 www/img/icon_48.png
