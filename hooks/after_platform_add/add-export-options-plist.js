a98a44900efe3086a79623768f4e4c5f0968b993
Mike Sinn <m@quantimodo.com>
2016-05-14 10:46:22 -0500

    Only execute iOS hooks for iOS platform

--- hooks/after_platform_add/add-export-options-plist.js
+++ hooks/after_platform_add/add-export-options-plist.js
@@ -6,23 +6,32 @@
 var path = require('path');
 var fs = require('fs');
 var TEAM_ID = process.env.TEAM_ID;
+var platforms = process.env.CORDOVA_PLATFORMS;
+var rootdir = process.argv[2];
+console.log('hook: root directory is ' + rootdir);
 
-var plistFile = path.resolve(__dirname, '../../platforms/ios/exportOptions.plist');
+if (platforms == "ios") {
+	var plistFile = path.resolve(__dirname, '../../platforms/ios/exportOptions.plist');
 
-var result = [
-	'<?xml version="1.0" encoding="UTF-8"?>',
-	'<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">',
-	'<plist version="1.0">',
-	'<dict>',
-	'<key>method</key>',
-	'<string>app-store</string>',
-	'<key>teamID</key>',
-	'<string>'+TEAM_ID+'</string>',
-	'</dict>',
-	'</plist>'
-].join('\n');
+	var result = [
+		'<?xml version="1.0" encoding="UTF-8"?>',
+		'<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">',
+		'<plist version="1.0">',
+		'<dict>',
+		'<key>method</key>',
+		'<string>app-store</string>',
+		'<key>teamID</key>',
+		'<string>'+TEAM_ID+'</string>',
+		'</dict>',
+		'</plist>'
+	].join('\n');
 
-fs.writeFile(plistFile, result, 'utf8', function (err) {
-	if (err) return console.log('No directory Found for cordova iOS! Skipping exportOptionsPlist creation. ',err);
-	console.log('Cordova iOS exportOptionsPlist updated with profile and developer values.')
-});
+	fs.writeFile(plistFile, result, 'utf8', function (err) {
+		if (err) return console.log('No directory Found for cordova iOS! Skipping exportOptionsPlist creation. ',err);
+		console.log('Cordova iOS exportOptionsPlist updated with profile and developer values.')
+	});
+} else if (platforms == "android") {
+	console.log("add-export-options hook is for iOS only");
+} else {
+  console.log("dd-export-options hook is for iOS only");
+}
--- hooks/after_platform_add/update_xcconfig.js
+++ hooks/after_platform_add/update_xcconfig.js
@@ -6,17 +6,27 @@
  */
 var path = require('path');
 var fs = require('fs');
+var rootdir = process.argv[2];
+console.log('hook: root directory is ' + rootdir);
+var platforms = process.env.CORDOVA_PLATFORMS;
 
-var xcconfigFinal = path.resolve(__dirname, '../../platforms/ios/cordova/build.xcconfig');
-var xcconfigTemplate = path.resolve(__dirname, '../../scripts/xcconfig/build.xcconfig.template');
 
-fs.readFile(xcconfigTemplate, 'utf8', function (err,templateFile) {
-  if (err) {
-    return console.log(err);
-  }
+if (platforms == "ios") {
+  var xcconfigFinal = path.resolve(__dirname, '../../platforms/ios/cordova/build.xcconfig');
+  var xcconfigTemplate = path.resolve(__dirname, '../../scripts/xcconfig/build.xcconfig.template');
 
-  fs.writeFile(xcconfigFinal, templateFile, 'utf8', function (err) {
-    if (err) return console.log('No directory Found for cordova iOS! Skipping xcconfig creation. ',err);
-    console.log('Cordova iOS build.xcconfig replaced successfully.')
+  fs.readFile(xcconfigTemplate, 'utf8', function (err,templateFile) {
+    if (err) {
+      return console.log(err);
+    }
+
+    fs.writeFile(xcconfigFinal, templateFile, 'utf8', function (err) {
+      if (err) return console.log('No directory Found for cordova iOS! Skipping xcconfig creation. ',err);
+      console.log('Cordova iOS build.xcconfig replaced successfully.')
+    });
   });
-});
+} else if (platforms == "android") {
+	console.log("update_xconfig hook is for iOS only");
+} else {
+  console.log("update_xconfig hook is for iOS only");
+}
