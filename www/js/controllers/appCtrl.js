c03e2a019311530fee284521119d54b952876488
Mike P. Sinn <m@quantimodo.com>
2016-05-18 19:43:34 -0500

    $scope.safeApply

--- www/js/controllers/appCtrl.js
+++ www/js/controllers/appCtrl.js
@@ -365,6 +365,17 @@ angular.module('starter')
             return userObject;
         };
 
+        $scope.safeApply = function(fn) {
+            var phase = this.$root.$$phase;
+            if(phase === '$apply' || phase === '$digest') {
+                if(fn && (typeof(fn) === 'function')) {
+                    fn();
+                }
+            } else {
+                this.$apply(fn);
+            }
+        };
+
 
         // call constructor
         $scope.init();
