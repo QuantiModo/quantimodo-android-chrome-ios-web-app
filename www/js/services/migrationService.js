883de30b30b56dac926e3cefef276a9d60bcf064
Mike Sinn <m@quantimodo.com>
2016-06-10 14:47:56 -0500

    1.4.8.11: Backup measurments before migration

--- www/js/services/migrationService.js
+++ www/js/services/migrationService.js
@@ -8,17 +8,22 @@ angular.module('starter')
 			version1466 : function(){
                 var storedVersion = localStorageService.getItemAsObject('appVersion');
                 if (!$rootScope.user && !storedVersion) {
-                    localStorageService.setItem('appVersion', 1466);
+                    localStorageService.setItem('appVersion', 1489);
                 }
-                else if (storedVersion < 1466){
-                    console.debug('Running migration version version1466...');
+                else if (storedVersion < 1489){
+                    console.debug('Running migration version version1489...');
+                    localStorageService.getItem('allMeasurements',function(allMeasurements) {
+                        Bugsnag.user = $rootScope.user;
+                        Bugsnag.notify('Backing up user measurements', allMeasurements, {}, "error");
+                        localStorageService.setItem('allMeasurementsBackup1489', allMeasurements);
+                    });
                     localStorageService.deleteItem('allMeasurements');
                     localStorageService.deleteItem('lastSyncTime');
                     measurementService.syncPrimaryOutcomeVariableMeasurements().then(function(){
                         console.log("Measurement sync complete!");
                         $rootScope.isSyncing = false;
                     });
-                    localStorageService.setItem('appVersion', 1466);
+                    localStorageService.setItem('appVersion', 1489);
                 }
 			}
 		};
