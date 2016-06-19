357ee8f15ecfaadc866528e2422190d1f7badd26
Mike Sinn <m@quantimodo.com>
2016-06-18 19:02:20 -0500

    1.5.3.2

--- scripts/build_all_apps.sh
+++ scripts/build_all_apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export IONIC_APP_VERSION_NUMBER=1.5.3
-export IONIC_IOS_APP_VERSION_NUMBER="1.5.3.1"
+export IONIC_IOS_APP_VERSION_NUMBER="1.5.3.2"
 
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
