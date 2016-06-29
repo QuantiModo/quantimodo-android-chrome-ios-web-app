4206d1a07fd993b7b4b7432eeeb7770277043817
Mike Sinn <m@quantimodo.com>
2016-06-29 14:43:57 -0500

    Added ionic deploy

--- www/js/controllers/settingsCtrl.js
+++ www/js/controllers/settingsCtrl.js
@@ -16,6 +16,29 @@ angular.module('starter')
         $rootScope.isChrome = window.chrome ? true : false;
 	    // populate user data
 
+		var deploy = new Ionic.Deploy();
+
+		// Update app code with new release from Ionic Deploy
+		$scope.doUpdate = function() {
+			deploy.update().then(function(res) {
+				console.log('Ionic Deploy: Update Success! ', res);
+			}, function(err) {
+				console.log('Ionic Deploy: Update error! ', err);
+			}, function(prog) {
+				console.log('Ionic Deploy: Progress... ', prog);
+			});
+		};
+
+		// Check Ionic Deploy for new code
+		$scope.checkForUpdates = function() {
+			console.log('Ionic Deploy: Checking for updates');
+			deploy.check().then(function(hasUpdate) {
+				console.log('Ionic Deploy: Update available: ' + hasUpdate);
+				$scope.hasUpdate = hasUpdate;
+			}, function(err) {
+				console.error('Ionic Deploy: Unable to check for updates', err);
+			});
+		};
 
         // when login is tapped
 	    $scope.loginFromSettings = function(){
