a5de81d1c8ee5c81ee76902860623b3b6d7543b4
Mike Sinn <m@quantimodo.com>
2016-07-17 14:54:26 -0500

    Added qmLocationService

--- www/js/controllers/appCtrl.js
+++ www/js/controllers/appCtrl.js
@@ -5,7 +5,8 @@ angular.module('starter')
                                     measurementService, $ionicPopover, $ionicLoading, $state, $ionicHistory,
                                     QuantiModo, notificationService, $rootScope, localStorageService, reminderService,
                                     $ionicPopup, $ionicSideMenuDelegate, ratingService, migrationService,
-                                    ionicDatePicker, unitService, variableService, $ionicPlatform, $cordovaGeolocation) {
+                                    ionicDatePicker, unitService, variableService, $ionicPlatform, $cordovaGeolocation,
+                                    qmLocationService) {
 
         $rootScope.loaderImagePath = config.appSettings.loaderImagePath;
         $scope.appVersion = 1489;
@@ -52,6 +53,21 @@ angular.module('starter')
                         $rootScope.latitude  = position.coords.latitude;
                         $rootScope.longitude = position.coords.longitude;
 
+                        qmLocationService.getInfo($rootScope.longitude, $rootScope.latitude).then(function(result) {
+                            console.log('Result was '+JSON.stringify(result));
+                            if(result.type === 'foursquare') {
+                                console.log('Your photo was taken at ' + result.name + ' located at ' + result.address);
+                            } else if (result.type === 'geocode') {
+                                console.log('Your photo appears to have been taken at ' + result.address);
+                            } else {
+                                var map = 'https://maps.googleapis.com/maps/api/staticmap?center='+
+                                    $rootScope.latitude+','+$rootScope.longitude+
+                                    'zoom=13&size=300x300&maptype=roadmap&markers=color:blue%7Clabel:X%7C'+
+                                    $rootScope.latitude+','+$rootScope.longitude;
+                                console.log('Sorry, I\'ve got nothing. But here is a map!');
+                            }
+                        });
+
                         console.debug("My coordinates are: ", position.coords);
                         $ionicLoading.hide();
 
@@ -60,41 +76,6 @@ angular.module('starter')
                         console.log(err);
                     });
 
-                    /**
-                     * This callback will be executed every time a geolocation is recorded in the background.
-                     */
-                    var callbackFn = function(location) {
-                        console.log('[js] BackgroundGeolocation callback:  ' + location.latitude + ',' + location.longitude);
-
-                        // Do your HTTP request here to POST location to your server.
-                        // jQuery.post(url, JSON.stringify(location));
-
-                        /*
-                         IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
-                         and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
-                         IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
-                         */
-                        backgroundGeolocation.finish();
-                    };
-
-                    var failureFn = function(error) {
-                        console.log('BackgroundGeolocation error');
-                    };
-
-                    // BackgroundGeolocation is highly configurable. See platform specific configuration options
-                    backgroundGeolocation.configure(callbackFn, failureFn, {
-                        desiredAccuracy: 10,
-                        stationaryRadius: 20,
-                        distanceFilter: 30,
-                        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
-                        stopOnTerminate: false, // <-- enable this to clear background location settings when the app terminates
-                    });
-
-                    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
-                    backgroundGeolocation.start();
-
-                    // If you wish to turn OFF background-tracking, call the #stop method.
-                    // backgroundGeolocation.stop();
                 });
             }
         };
