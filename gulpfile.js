d2a70d5857f5f27d9f693a9fa4a86a7638231475
Mike Sinn <m@quantimodo.com>
2016-07-18 11:58:33 -0500

    1.6.8.7

--- gulpfile.js
+++ gulpfile.js
@@ -1033,7 +1033,7 @@ gulp.task('setVersionNumbersWithEnvs', function(){
 
 	if(!environmentalVariables['IONIC_IOS_APP_VERSION_NUMBER']){
 		//throw new Error('Please set IONIC_IOS_APP_VERSION_NUMBER env!');
-		environmentalVariables['IONIC_IOS_APP_VERSION_NUMBER'] = '1.6.8.6';
+		environmentalVariables['IONIC_IOS_APP_VERSION_NUMBER'] = '1.6.8.7';
 		console.warn('No IONIC_IOS_APP_VERSION_NUMBER env!  Using hardcoded gulp version ' +
 			environmentalVariables['IONIC_IOS_APP_VERSION_NUMBER'])
 	}
