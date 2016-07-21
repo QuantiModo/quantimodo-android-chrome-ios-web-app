57d2612718e299544b354cf23cbc6386eaee70df
Mike Sinn <m@quantimodo.com>
2016-07-21 13:56:03 -0500

    1.7.3.0

--- www/js/app.js
+++ www/js/app.js
@@ -66,7 +66,7 @@ angular.module('starter',
                 return;
             }
 
-            $rootScope.appVersion = "1.7.2.5";
+            $rootScope.appVersion = "1.7.3.0";
 
             if(window.private_keys.bugsnag_key) {
                 //Set Bugsnag Release Stage
