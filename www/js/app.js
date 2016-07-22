e4cbcef28a99e5d27ad52a4dbb6debbbe6a57845
Mike P. Sinn <m@quantimodo.com>
2016-07-22 11:09:38 -0500

    1.7.4.2 Dismiss

--- www/js/app.js
+++ www/js/app.js
@@ -66,7 +66,7 @@ angular.module('starter',
                 return;
             }
 
-            $rootScope.appVersion = "1.7.4.1";
+            $rootScope.appVersion = "1.7.4.2";
 
             if(window.private_keys.bugsnag_key) {
                 //Set Bugsnag Release Stage
