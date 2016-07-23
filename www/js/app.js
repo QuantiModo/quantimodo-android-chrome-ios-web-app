8214f86fe623a8fb011dda4340ab18fe72e34280
Mike P. Sinn <m@quantimodo.com>
2016-07-23 00:04:34 -0500

    1.7.5.0 Shorter intro tours

--- www/js/app.js
+++ www/js/app.js
@@ -66,7 +66,7 @@ angular.module('starter',
                 return;
             }
 
-            $rootScope.appVersion = "1.7.5.0";
+            $rootScope.appVersion = "1.7.6.0";
 
             if(window.private_keys.bugsnag_key) {
                 //Set Bugsnag Release Stage
