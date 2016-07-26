60cf4a3f694915e9b8b21b90ad8a9f8c4a493f30
Mike Sinn <m@quantimodo.com>
2016-07-26 15:34:37 -0500

    Don't call ionicHistory if not defined

--- www/js/controllers/fabric.js
+++ www/js/controllers/fabric.js
@@ -1,10 +1,11 @@
-angular.module('fabric', ['ng']).config(['$provide', function($provide) {
+angular.module('fabric', ['ng']).config(['$provide', function($provide, $ionicHistory) {
   $provide.decorator("$exceptionHandler", ['$delegate', function($delegate, $ionicHistory, $state) {
     return function(exception, cause) {
       $delegate(exception, cause);
 
       // Decorating standard exception handling behaviour by sending exception to crashlytics plugin
       var message = exception.toString();
+      var ionicViewHistory = {};
       // Here, I rely on stacktrace-js (http://www.stacktracejs.com/) to format exception stacktraces before
       // sending it to the native bridge
       var stacktrace;
@@ -14,7 +15,11 @@ angular.module('fabric', ['ng']).config(['$provide', function($provide) {
          stacktrace = "No stack trace provided with exception";
       }
         Bugsnag.apiKey = window.private_keys.bugsnag_key;
-        var ionicViewHistory = $ionicHistory.viewHistory();
+      if(typeof $ionicHistory !== "undefined"){
+        ionicViewHistory = $ionicHistory.viewHistory();
+      } else {
+        ionicViewHistory = "$ionicHistory is undefined in fabric.js";
+      }
         Bugsnag.notify("ERROR: "+$state.current.name, "ionic history backView: "+ JSON.stringify(ionicViewHistory), ionicViewHistory, "error");
       Bugsnag.notify("ERROR: "+message, "Stacktrace: "+stacktrace, {}, "error");
       if(typeof navigator !== 'undefined' && typeof navigator.crashlytics !== 'undefined'){
