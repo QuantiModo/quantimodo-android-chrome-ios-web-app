8e3688a9a10fa91b283e332311562667327c3f64
Mike P. Sinn <m@quantimodo.com>
2016-07-26 21:21:22 -0500

    Less verbose error logging

--- www/js/controllers/fabric.js
+++ www/js/controllers/fabric.js
@@ -5,7 +5,6 @@ angular.module('fabric', ['ng']).config(['$provide', function($provide, $ionicHi
 
       // Decorating standard exception handling behaviour by sending exception to crashlytics plugin
       var message = exception.toString();
-      var ionicViewHistory = {};
       // Here, I rely on stacktrace-js (http://www.stacktracejs.com/) to format exception stacktraces before
       // sending it to the native bridge
       var stacktrace;
@@ -15,16 +14,6 @@ angular.module('fabric', ['ng']).config(['$provide', function($provide, $ionicHi
          stacktrace = "No stack trace provided with exception";
       }
         Bugsnag.apiKey = window.private_keys.bugsnag_key;
-      if(typeof $ionicHistory !== "undefined"){
-        ionicViewHistory = $ionicHistory.viewHistory();
-      } else {
-        ionicViewHistory = "$ionicHistory is undefined in fabric.js";
-      }
-      var currentState = 'Could not determine current state';
-      if(typeof $state !== "undefined" && typeof $state.current !== "undefined" && typeof $state.current.name !== "undefined"){
-        currentState = $state.current.name;
-      }
-      Bugsnag.notify("ERROR: "+ currentState, "ionic history backView: "+ JSON.stringify(ionicViewHistory), ionicViewHistory, "error");
       Bugsnag.notify("ERROR: "+message, "Stacktrace: "+stacktrace, {}, "error");
       if(typeof navigator !== 'undefined' && typeof navigator.crashlytics !== 'undefined'){
         navigator.crashlytics.logException("ERROR: "+message+", stacktrace: "+stacktrace);
--- www/js/services/bugsnagService.js
+++ www/js/services/bugsnagService.js
@@ -7,7 +7,6 @@ angular.module('starter')
 
             reportError : function(exception){
                 var message = exception.toString();
-                var ionicViewHistory = {};
                 var stacktrace;
                 if(typeof exception.stack !== 'undefined'){
                     stacktrace = exception.stack.toLocaleString();
@@ -15,18 +14,6 @@ angular.module('starter')
                     stacktrace = "No stack trace provided with exception";
                 }
                 Bugsnag.apiKey = window.private_keys.bugsnag_key;
-                if(typeof $ionicHistory !== "undefined"){
-                    ionicViewHistory = $ionicHistory.viewHistory();
-                } else {
-                    ionicViewHistory = "$ionicHistory is undefined in fabric.js";
-                }
-
-                var currentState = 'Could not determine current state';
-                if(typeof $state !== "undefined" && typeof $state.current !== "undefined" && typeof $state.current.name !== "undefined"){
-                    currentState = $state.current.name;
-                }
-
-                Bugsnag.notify("ERROR: "+ currentState, "ionic history backView: "+ JSON.stringify(ionicViewHistory), ionicViewHistory, "error");
                 Bugsnag.notify("ERROR: "+message, "Stacktrace: "+stacktrace, {}, "error");
             }
         };
