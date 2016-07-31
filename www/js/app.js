5a9fdca8c8a17a41e0ff73632bbde912a630701b
Mike P. Sinn <m@quantimodo.com>
2016-07-31 10:13:30 -0500

    1.8.0.8

--- www/js/app.js
+++ www/js/app.js
@@ -66,7 +66,7 @@ angular.module('starter',
                 return;
             }
 
-            $rootScope.appVersion = "1.8.0.7";
+            $rootScope.appVersion = "1.8.0.8";
             $rootScope.appName = config.appSettings.appName;
 
             if(window.private_keys.bugsnag_key) {
