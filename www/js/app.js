1386bcd6246001ab46e2b7a17c2f5b9b5320bcae
Mike Sinn <m@quantimodo.com>
2016-08-04 09:27:58 -0500

    1.8.0.15 Try removing ionic, cordova, and push plugin version locks

--- www/js/app.js
+++ www/js/app.js
@@ -138,7 +138,7 @@ angular.module('starter',
                 return;
             }
 
-            $rootScope.appVersion = "1.8.0.14";
+            $rootScope.appVersion = "1.8.0.15";
             $rootScope.appName = config.appSettings.appName;
 
             if(window.private_keys.bugsnag_key) {
