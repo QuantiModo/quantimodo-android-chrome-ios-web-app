d7d20988fa1c7232f7c99e79796c94bb4d7d0251
Mike P. Sinn <m@quantimodo.com>
2016-08-06 11:11:06 -0500

    1.8.0.17 Hide multiple notifications disabled button

--- scripts/build_all_apps.sh
+++ scripts/build_all_apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export IONIC_APP_VERSION_NUMBER=1.8.0
-export IONIC_IOS_APP_VERSION_NUMBER="1.8.0.16"
+export IONIC_IOS_APP_VERSION_NUMBER="1.8.0.17"
 
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
