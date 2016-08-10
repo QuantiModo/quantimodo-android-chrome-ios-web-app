f7c9d8dd675e6ff5ad991e64d0601d9d6e67de67
Kelly Stathis <kelly.stathis@gmail.com>
2016-08-10 15:14:04 -0500

    Wait to go to favorites page until reminder/favorite is posted and reminders are refreshed, so that favorites have ids

--- www/js/controllers/appCtrl.js
+++ www/js/controllers/appCtrl.js
@@ -255,16 +255,20 @@ angular.module('starter')
                 reminderService.addNewReminder(trackingReminder)
                     .then(function () {
                         console.debug("Saved Reminder", trackingReminder);
+                        reminderService.refreshTrackingRemindersAndScheduleAlarms()
+                        .then(function() {
+                            $state.go('app.favorites',
+                                {
+                                    trackingReminder: trackingReminder,
+                                    fromState: $state.current.name,
+                                    fromUrl: window.location.href
+                                }
+                            );
+                        });
                     }, function (err) {
                         console.error('Failed to add Reminder!', trackingReminder);
                     });
-                $state.go('app.favorites',
-                    {
-                        trackingReminder: trackingReminder,
-                        fromState: $state.current.name,
-                        fromUrl: window.location.href
-                    }
-                );
+
             } else {
                 $state.go('app.favoriteAdd',
                     {
