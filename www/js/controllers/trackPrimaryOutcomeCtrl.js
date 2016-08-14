0bc3b68c49fe6ed6562fe90855d3a23760c16b8e
Mike Sinn <m@quantimodo.com>
2016-08-13 21:03:19 -0500

    Simplified primary outcome charts

--- www/js/controllers/trackPrimaryOutcomeCtrl.js
+++ www/js/controllers/trackPrimaryOutcomeCtrl.js
@@ -11,21 +11,7 @@ angular.module('starter')
         $scope.timeRemaining = false;
         $scope.averagePrimaryOutcomeVariableImage = false;
         $scope.averagePrimaryOutcomeVariableValue = false;
-        $scope.lineChartData = null;
-        $scope.distributionChartData = null;
         $scope.showRatingFaces = true;
-
-        // chart flags
-        $scope.state = {
-            showDistributionChart : false,
-            showLineChart : false,
-            showWeekdayChart: false,
-            showHourlyChart : false
-        };
-        $scope.lineChartConfig = false;
-        $scope.distributionChartConfig = false;
-        $scope.weekdayChartConfig = false;
-        $scope.hourlyChartConfig = false;
         $scope.syncDisplayText = 'Syncing ' + config.appSettings.primaryOutcomeVariableDetails.name + ' measurements...';
 
         $scope.storeRatingLocalAndServerAndUpdateCharts = function (numericRatingValue) {
@@ -46,8 +32,9 @@ angular.module('starter')
 
             if(!$rootScope.isSyncing){
                 $scope.showLoader($scope.syncDisplayText);
-                measurementService.syncPrimaryOutcomeVariableMeasurementsAndUpdateCharts().then(function() {
-                    $scope.hideLoader();
+                measurementService.syncPrimaryOutcomeVariableMeasurements().then(function(){
+                    updateCharts();
+                    $ionicLoading.hide();
                 });
             }
            
@@ -68,108 +55,24 @@ angular.module('starter')
                 $scope.safeApply();
             }
         };
-
-        var updateDistributionChart = function(distributionChartData){
-            console.log("Configuring bar chart...");
-            $scope.distributionChartConfig = chartService.configureDistributionChart(distributionChartData, config.appSettings.primaryOutcomeVariableDetails);
-            $scope.state.showDistributionChart = true;
-        };
-
-        var updateLineChart = function(lineChartData){
-            console.log("Configuring line chart...");
-            $scope.lineChartConfig = chartService.configureLineChart(lineChartData, config.appSettings.primaryOutcomeVariableDetails);
-            $scope.state.showLineChart = true;
-        };
-
-        var updateWeekdayChart = function(weekdayChartData){
-            console.log("Configuring Weekday chart...");
-            $scope.weekdayChartConfig = chartService.configureWeekdayChart(weekdayChartData, config.appSettings.primaryOutcomeVariableDetails);
-            $scope.state.showWeekdayChart = true;
-        };
-
-        var updateHourlyChart = function(hourlyChartData){
-            console.log("Configuring Hourly chart...");
-            $scope.hourlyChartConfig = chartService.configureHourlyChart(hourlyChartData, config.appSettings.primaryOutcomeVariableDetails);
-            $scope.state.showHourlyChart = true;
-        };
-
         var updateCharts = function(){
-
-            measurementService.getAllLocalMeasurements(false, function(allMeasurements) {
-                var lineArr = [];
-                var barArr = [0, 0, 0, 0, 0];
-                var sum = 0;
-                var weekdayMeasurementArrays = [];
-                var hourlyMeasurementArrays = [];
-                $scope.averageValueByWeekday = [];
-                $scope.averageValueByHour = [];
-
-                if (allMeasurements) {
-                    var fromDate = parseInt(localStorageService.getItemSync('fromDate'));
-                    var toDate = parseInt(localStorageService.getItemSync('toDate'));
-                    if (!fromDate) {
-                        fromDate = 0;
-                    }
-                    if (!toDate) {
-                        toDate = Date.now();
-                    }
-                    var rangeLength = 0; // number of measurements in date range
-                    for (var i = 0; i < allMeasurements.length; i++) {
-                        var currentValue = Math.ceil(allMeasurements[i].value);
-                        if (allMeasurements[i].abbreviatedUnitName ===
-                            config.appSettings.primaryOutcomeVariableDetails.abbreviatedUnitName &&
-                            (currentValue - 1) <= 4 && (currentValue - 1) >= 0) {
-                            var startTimeMilliseconds = allMeasurements[i].startTimeEpoch * 1000;
-                            if (startTimeMilliseconds >= fromDate && startTimeMilliseconds <= toDate) {
-                                var percentValue = (currentValue - 1) * 25;
-                                var lineChartItem = [startTimeMilliseconds, percentValue];
-                                lineArr.push(lineChartItem);
-                                barArr[currentValue - 1]++;
-
-                                sum+= allMeasurements[i].value;
-                                rangeLength++;
-                            }
-                            if(typeof weekdayMeasurementArrays[moment(startTimeMilliseconds).day()] === "undefined"){
-                                weekdayMeasurementArrays[moment(startTimeMilliseconds).day()] = [];
-                            }
-                            weekdayMeasurementArrays[moment(startTimeMilliseconds).day()].push(allMeasurements[i]);
-                            if(typeof hourlyMeasurementArrays[moment(startTimeMilliseconds).hour()] === "undefined"){
-                                hourlyMeasurementArrays[moment(startTimeMilliseconds).hour()] = [];
-                            }
-                            hourlyMeasurementArrays[moment(startTimeMilliseconds).hour()].push(allMeasurements[i]);
-                        }
-                    }
-
-                    $scope.averageValueByWeekday = chartService.calculateAverageValueByWeekday(weekdayMeasurementArrays);
-                    $scope.averageValueByHour = chartService.calculateAverageValueByHour(hourlyMeasurementArrays);
-
-                    var averagePrimaryOutcomeVariableValue = Math.round(sum/(rangeLength));
-                    localStorageService.setItem('averagePrimaryOutcomeVariableValue',averagePrimaryOutcomeVariableValue);
-
-                    if(!$scope.distributionChartConfig || barArr !== $scope.distributionChartConfig.series[0].data) {
-                        updateAveragePrimaryOutcomeRatingView(averagePrimaryOutcomeVariableValue);
-                        $scope.lineChartData = lineArr;
-                        $scope.distributionChartData = barArr;
-                        if ($scope.lineChartData.length > 0 && $scope.distributionChartData.length === 5) {
-                            updateLineChart($scope.lineChartData);
-                            updateDistributionChart($scope.distributionChartData);
-                            updateWeekdayChart($scope.averageValueByWeekday);
-                            updateHourlyChart($scope.averageValueByHour);
-                            $scope.showCharts = true;
-                        }
-                        if (!$scope.$$phase) {
-                            $scope.safeApply();
-                        }
-                    }
-                    $(window).resize();
-                    $timeout(function() {
-                        $scope.$broadcast('highchartsng.reflow');
-                    }, 10);
-                    // Fixes chart width
-                    $scope.$broadcast('highchartsng.reflow');
+            measurementService.getAllLocalMeasurements(false, function(primaryOutcomeMeasurements) {
+                primaryOutcomeMeasurements = JSON.parse(primaryOutcomeMeasurements);
+                if (primaryOutcomeMeasurements) {
+                    $scope.lineChartConfig =
+                        chartService.processDataAndConfigureLineChart(primaryOutcomeMeasurements,
+                            config.appSettings.primaryOutcomeVariableDetails);
+                    $scope.hourlyChartConfig =
+                        chartService.processDataAndConfigureHourlyChart(primaryOutcomeMeasurements,
+                            config.appSettings.primaryOutcomeVariableDetails);
+                    $scope.weekdayChartConfig =
+                        chartService.processDataAndConfigureWeekdayChart(primaryOutcomeMeasurements,
+                            config.appSettings.primaryOutcomeVariableDetails);
+                    $scope.distributionChartConfig =
+                        chartService.processDataAndConfigureDistributionChart(primaryOutcomeMeasurements,
+                            config.appSettings.primaryOutcomeVariableDetails);
                 }
             });
-
         };
 
 
@@ -177,28 +80,22 @@ angular.module('starter')
             $ionicLoading.hide();
             Bugsnag.context = "trackPrimary";
             updateCharts();
-
-            $scope.showHelpInfoPopupIfNecessary();
-            if (typeof analytics !== 'undefined')  { analytics.trackView("Track Primary Outcome Controller"); }
-            $ionicLoading.hide();
-        };
-
-        $scope.init();
-        
-        $scope.$on('updateCharts', function(){
-            $scope.hideLoader();
-            console.log('track state redrawing event triggered through sibling controllers. Updating charts and syncing..');
-            updateCharts();
-        });
-
-        $scope.$on('$ionicView.enter', function(e) {
-            $scope.hideLoader();
             console.log('Track state brought in focus. Updating charts and syncing..');
             $scope.showRatingFaces = true;
             $scope.timeRemaining = false;
             $scope.showLoader($scope.syncDisplayText);
-            measurementService.syncPrimaryOutcomeVariableMeasurementsAndUpdateCharts().then(function() {
-                $scope.hideLoader();
+            measurementService.syncPrimaryOutcomeVariableMeasurements().then(function(){
+                updateCharts();
+                $ionicLoading.hide();
             });
+            if (typeof analytics !== 'undefined')  { analytics.trackView("Track Primary Outcome Controller"); }
+        };
+
+        $scope.$on('updateCharts', function(){
+            $scope.init();
+        });
+
+        $scope.$on('$ionicView.enter', function(e) {
+            $scope.init();
         });
     });
