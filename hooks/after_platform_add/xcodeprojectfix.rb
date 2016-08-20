384720c3ab33d5c6817c8983054e3e2c89228d82
Mike P. Sinn <m@quantimodo.com>
2016-08-20 10:15:59 -0500

    Copied config from visual studio template

--- hooks/after_platform_add/xcodeprojectfix.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-require 'xcodeproj'
-xcodeprojPath = "platforms/ios/" + ENV['APP_DISPLAY_NAME'] + ".xcodeproj"
-xcproj = Xcodeproj::Project.open(xcodeprojPath)
-xcproj.recreate_user_schemes
-xcproj.save
