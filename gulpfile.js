4c5b214f7b6c9cb487776b64a643e66003d7d584
Mike Sinn <m@quantimodo.com>
2016-08-20 18:18:12 -0500

    Updated config.xml and package.json with VS template

--- gulpfile.js
+++ gulpfile.js
@@ -27,4 +27,26 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
 
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
+});
