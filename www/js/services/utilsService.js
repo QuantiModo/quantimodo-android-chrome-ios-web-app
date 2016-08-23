3959c488105a990df43523f82dc161688bad1f7d
Mike P. Sinn <m@quantimodo.com>
2016-08-22 22:29:12 -0500

    localStorageService

--- www/js/services/utilsService.js
+++ www/js/services/utilsService.js
@@ -1,7 +1,7 @@
 angular.module('starter')
 
     // utility methods
-    .factory('utilsService', function($ionicPopup, $ionicLoading, $rootScope) {
+    .factory('utilsService', function($ionicPopup, $ionicLoading, $rootScope, localStorageService) {
 
         return {
 
