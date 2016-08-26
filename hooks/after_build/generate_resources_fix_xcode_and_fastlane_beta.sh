df4b6b12306df8c45587dbcf4959d2a709ffbb43
Mike Sinn <m@quantimodo.com>
2016-08-25 21:24:09 -0500

    Updated hooks\after_build\generate_resources_fix_xcode_and_fastlane_beta.sh

--- hooks/after_build/generate_resources_fix_xcode_and_fastlane_beta.sh
+++ hooks/after_build/generate_resources_fix_xcode_and_fastlane_beta.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 npm rebuild node-sass
 ionic resources
-export APP_DISPLAY_NAME=MoodiModo
+if [ -z "${APP_DISPLAY_NAME}" ]
+    then
+      export APP_DISPLAY_NAME=MoodiModo
+      echo -e "${RED}APP_DISPLAY_NAME env is not defined so assuming it to be ${APP_DISPLAY_NAME}${NC}"
+      exit 1
+fi
 ruby hooks/after_platform_add.bak/xcodeprojectfix.rb
 fastlane beta
