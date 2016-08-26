fa48c097b62fcffece6c61e653c533b3d8cd83ee
Mike Sinn <m@quantimodo.com>
2016-08-25 21:46:53 -0500

    -exit 1

--- hooks/after_build/generate_resources_fix_xcode_and_fastlane_beta.sh
+++ hooks/after_build/generate_resources_fix_xcode_and_fastlane_beta.sh
@@ -5,7 +5,6 @@ if [ -z "${APP_DISPLAY_NAME}" ]
     then
       export APP_DISPLAY_NAME=MoodiModo
       echo -e "${RED}APP_DISPLAY_NAME env is not defined so assuming it to be ${APP_DISPLAY_NAME}${NC}"
-      exit 1
 fi
 ruby hooks/after_platform_add.bak/xcodeprojectfix.rb
 fastlane beta
