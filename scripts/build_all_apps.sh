6d18b31ef60ce167d27f83110405fe8fda9cefd4
Mike Sinn <m@quantimodo.com>
2016-08-29 12:06:41 -0500

    MAKE SURE NOT TO USE QUOTES WITH export APP_DESCRIPTION OR IT WILL NOT REPLACE PROPERLY

--- scripts/build_all_apps.sh
+++ scripts/build_all_apps.sh
@@ -95,7 +95,7 @@ export APPLE_ID="1115037060"
 export APP_IDENTIFIER="com.quantimodo.quantimodo"
 export APP_DISPLAY_NAME="QuantiModo"
 export LOWERCASE_APP_NAME=quantimodo
-export APP_DESCRIPTION="Perfect your life!"
+export APP_DESCRIPTION=Perfect your life!
 
 echo "Moving old ${LOWERCASE_APP_NAME} Android versions to archive so we catch build failures"
 mv ${DROPBOX_PATH}/QuantiModo/apps/${LOWERCASE_APP_NAME}/android/* ${DROPBOX_PATH}/QuantiModo/apps/${LOWERCASE_APP_NAME}/android/archive/
@@ -118,7 +118,7 @@ export APPLE_ID="1046797567"
 export APP_IDENTIFIER="com.quantimodo.moodimodoapp"
 export APP_DISPLAY_NAME="MoodiModo"
 export LOWERCASE_APP_NAME=moodimodo
-export APP_DESCRIPTION="The mood-tracking app from QuantiModo"
+export APP_DESCRIPTION=The mood-tracking app from QuantiModo
 
 echo "Moving old ${LOWERCASE_APP_NAME} Android versions to archive so we catch build failures"
 mv ${DROPBOX_PATH}/QuantiModo/apps/${LOWERCASE_APP_NAME}/android/* ${DROPBOX_PATH}/QuantiModo/apps/${LOWERCASE_APP_NAME}/android/archive/
@@ -141,7 +141,7 @@ export APPLE_ID="1024924226"
 export APP_IDENTIFIER="com.quantimodo.moodimodo"
 export APP_DISPLAY_NAME="Mind First"
 export LOWERCASE_APP_NAME=mindfirst
-export APP_DESCRIPTION="The Mind First Mood Tracking app from QuantiModo"
+export APP_DESCRIPTION=The Mind First Mood Tracking app from QuantiModo
 
 echo "Moving old ${LOWERCASE_APP_NAME} Android versions to archive so we catch build failures"
 mv ${DROPBOX_PATH}/QuantiModo/apps/${LOWERCASE_APP_NAME}/android/* ${DROPBOX_PATH}/QuantiModo/apps/${LOWERCASE_APP_NAME}/android/archive/
@@ -163,7 +163,7 @@ export APPLE_ID="1115037652"
 export APP_IDENTIFIER="com.quantimodo.energymodo"
 export APP_DISPLAY_NAME="EnergyModo"
 export LOWERCASE_APP_NAME=energymodo
-export APP_DESCRIPTION="The energy-tracking app from QuantiModo"
+export APP_DESCRIPTION=The energy-tracking app from QuantiModo
 
 echo "Moving old ${LOWERCASE_APP_NAME} Android versions to archive so we catch build failures"
 mv ${DROPBOX_PATH}/QuantiModo/apps/${LOWERCASE_APP_NAME}/android/* ${DROPBOX_PATH}/QuantiModo/apps/${LOWERCASE_APP_NAME}/android/archive/
@@ -185,7 +185,7 @@ export APPLE_ID="1115037661"
 export APP_IDENTIFIER="com.quantimodo.medtlc"
 export APP_DISPLAY_NAME="MedTLC"
 export LOWERCASE_APP_NAME=medtlc
-export APP_DESCRIPTION="Medication - Track. Learn. Connect."
+export APP_DESCRIPTION=Medication - Track. Learn. Connect.
 
 echo "Moving old ${LOWERCASE_APP_NAME} Android versions to archive so we catch build failures"
 mv ${DROPBOX_PATH}/QuantiModo/apps/${LOWERCASE_APP_NAME}/android/* ${DROPBOX_PATH}/QuantiModo/apps/${LOWERCASE_APP_NAME}/android/archive/
