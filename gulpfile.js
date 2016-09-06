62a881f7072df2a6dc08bfc6055607fb0836b4dc
Mike Sinn <m@quantimodo.com>
2016-09-06 18:16:42 -0500

    1.9.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1065,14 +1065,14 @@ gulp.task('setVersionNumbersWithEnvs', function(){
 	var environmentalVariables = process.env;
 	if(!environmentalVariables.IONIC_APP_VERSION_NUMBER){
 		//throw new Error('Please set IONIC_APP_VERSION_NUMBER env!');
-		environmentalVariables.IONIC_APP_VERSION_NUMBER = '1.8.9';
+		environmentalVariables.IONIC_APP_VERSION_NUMBER = '1.9.0';
 		console.log('No IONIC_APP_VERSION_NUMBER env!  Using hardcoded gulp version number ' +
 			environmentalVariables.IONIC_APP_VERSION_NUMBER);
 	}
 
 	if(!environmentalVariables.IONIC_IOS_APP_VERSION_NUMBER){
 		//throw new Error('Please set IONIC_IOS_APP_VERSION_NUMBER env!');
-		environmentalVariables.IONIC_IOS_APP_VERSION_NUMBER = '1.8.9.0';
+		environmentalVariables.IONIC_IOS_APP_VERSION_NUMBER = '1.9.0.0';
 		console.log('No IONIC_IOS_APP_VERSION_NUMBER env!  Using hardcoded gulp version number ' +
 			environmentalVariables.IONIC_IOS_APP_VERSION_NUMBER);
 	}
