2db0437e983fd8f91fea062870cb19ca1b82d26a
Mike Sinn <m@quantimodo.com>
2016-09-18 17:55:02 -0500

    Fix random number generator for Android action buttons

--- scripts/build_scripts/push_plugin_install.sh
+++ scripts/build_scripts/push_plugin_install.sh
@@ -26,7 +26,7 @@ bower install --save-dev ionic-platform-web-client
 cordova plugin remove phonegap-plugin-push
 ionic plugin remove phonegap-plugin-push
 
-cordova plugin add https://github.com/mikepsinn/phonegap-plugin-push#e848085ecc76fd941628fb4c06a79cffacf08fff --variable SENDER_ID="${GCM_SENDER_ID}"
+cordova plugin add https://github.com/mikepsinn/phonegap-plugin-push#19c179e98347b4d4a09192becdd10fb0a890107b --variable SENDER_ID="${GCM_SENDER_ID}"
 #cordova plugin add phonegap-plugin-push --variable SENDER_ID="${GCM_SENDER_ID}"
 
 ionic io init -email ${IONIC_EMAIL} --password ${IONIC_PASSWORD}
