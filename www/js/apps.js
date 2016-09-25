82009f0cbc7e635aab741ae60f0019cfaa83c3be
Yi Yuan <yi.yuan.eu@gmail.com>
2016-09-26 01:09:06 +0300

    the config js file of own app

--- www/js/apps.js
+++ www/js/apps.js
@@ -5,11 +5,8 @@ var appsManager = { // jshint ignore:line
 		"mindfirst" : "configs/mindfirst",
 		"medtlc" : "configs/medtlc",
 		"quantimodo" : "configs/quantimodo",
-<<<<<<< Updated upstream
-		"supercell" : "configs/supercell",
-=======
 		"supercell" : "configs/supercell"
->>>>>>> Stashed changes
+
 	},
 	defaultApp : "supercell",
 	getDefaultConfig : function(){
