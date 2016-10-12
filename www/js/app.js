bb5857382b2c630eac4b87fae111bd80b0ba7026
Mike P. Sinn <m@quantimodo.com>
2016-10-12 10:00:22 -0500

    Ionic 2.0.4.0

--- www/js/app.js
+++ www/js/app.js
@@ -272,7 +272,7 @@ angular.module('starter',
                 return;
             }
 
-            $rootScope.appVersion = "2.0.3.0";
+            $rootScope.appVersion = "2.0.4.0";
             $rootScope.appName = config.appSettings.appName;
 
             if (typeof Bugsnag !== "undefined") {
