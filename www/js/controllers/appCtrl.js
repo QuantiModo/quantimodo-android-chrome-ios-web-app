b241bbe63a4f52c0e26684c6ad1135878b8f5416
Mike Sinn <m@quantimodo.com>
2016-10-19 19:27:14 -0500

    Added daily email reminder option to welcome setup

--- www/js/controllers/appCtrl.js
+++ www/js/controllers/appCtrl.js
@@ -409,7 +409,7 @@ angular.module('starter')
                 QuantiModo.refreshUser().then(function(){
                     $scope.syncEverything();
                 }, function(error){
-                    console.error('AppCtrl.init could not refresh user because ' + error);
+                    console.error('AppCtrl.init could not refresh user because ' + JSON.stringify(error));
                 });
             }
 
