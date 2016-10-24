94d09e110b32450d5f970d808c438bdc880e2bd1
Mike Sinn <m@quantimodo.com>
2016-10-23 19:46:43 -0500

    Nicer MoodiModo intro tour

--- www/js/controllers/introCtrl.js
+++ www/js/controllers/introCtrl.js
@@ -8,15 +8,22 @@ angular.module('starter')
 
     $scope.myIntro = {
         ready : false,
-
+        backgroundColor : 'white',
+        textColor : 'black',
         slideIndex : 0,
         // Called to navigate to the main app
         startApp : function() {
             console.debug('startApp: Going to welcome state...');
+            $rootScope.hideNavigationMenu = false;
             $state.go(config.appSettings.welcomeState);
         },
 
-        next : function() {
+        next : function(index) {
+            if(index === $scope.introSlides.length - 1){
+                console.debug('startApp: Going to welcome state...');
+                $rootScope.hideNavigationMenu = false;
+                $state.go(config.appSettings.welcomeState);
+            }
             $ionicSlideBoxDelegate.next();
         },
 
@@ -27,11 +34,23 @@ angular.module('starter')
         // Called each time the slide changes
         slideChanged : function(index) {
             $scope.myIntro.slideIndex = index;
+            if($scope.introSlides[index].backgroundColor){
+                $scope.myIntro.backgroundColor = $scope.introSlides[index].backgroundColor;
+            }
+            if($scope.introSlides[index].textColor){
+                $scope.myIntro.textColor = $scope.introSlides[index].textColor;
+            }
         }
     };
 
     // when view is changed
     $scope.$on('$ionicView.beforeEnter', function(e) { console.debug("Entering state " + $state.current.name);
+        if($scope.introSlides[0].backgroundColor){
+            $scope.myIntro.backgroundColor = $scope.introSlides[0].backgroundColor;
+        }
+        if($scope.introSlides[0].textColor){
+            $scope.myIntro.textColor = $scope.introSlides[0].textColor;
+        }
         if(QuantiModo.getAccessTokenFromUrlParameter()){
             console.debug('introCtrl beforeEnter: Skipping to default state: ' + config.appSettings.defaultState);
             $state.go(config.appSettings.defaultState);
@@ -41,6 +60,7 @@ angular.module('starter')
             if (typeof Bugsnag !== "undefined") { Bugsnag.context = $state.current.name; }
             if (typeof analytics !== 'undefined')  { analytics.trackView($state.current.name); }
             $scope.myIntro.ready = true;
+            $rootScope.hideNavigationMenu = true;
         }
         localStorage.setItem('introSeen', true);
     });
