6656c0e21ae9e1ca1a321e26ca93abbd534c59a1
Mike Sinn <m@quantimodo.com>
2016-10-26 17:59:22 -0500

    Added QM preparation script

---
+++ apps/moodimodo/hooks/generate_resources_fix_xcode_and_fastlane_beta.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+npm rebuild node-sass
+ionic resources
+if [ -z "${APP_DISPLAY_NAME}" ]
+    then
+      export APP_DISPLAY_NAME=MoodiModo
+      echo -e "${RED}APP_DISPLAY_NAME env is not defined so assuming it to be ${APP_DISPLAY_NAME}${NC}"
+fi
+if [ -z "${LOWERCASE_APP_NAME}" ]
+    then
+      export LOWERCASE_APP_NAME=moodimodo
+      echo -e "${RED}LOWERCASE_APP_NAME env is not defined so assuming it to be ${LOWERCASE_APP_NAME}${NC}"
+fi
+ruby hooks/after_platform_add.bak/xcodeprojectfix.rb
+# Commented due to podfile.lock error
+#npm install
+#gulp addBugsnagInObjC
+#gulp enableBitCode
+#gulp addDeploymentTarget
+#gulp addPodfile
+#gulp installPods
+fastlane beta
