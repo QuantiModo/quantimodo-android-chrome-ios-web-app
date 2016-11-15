e58daf3f92e6361a1fdf2a725702a6cb31867025
Mike P. Sinn <m@quantimodo.com>
2016-11-14 20:50:08 -0600

    Go to default state instead of back 2 when we came from a search state

--- www/js/controllers/remindersAddCtrl.js
+++ www/js/controllers/remindersAddCtrl.js
@@ -488,7 +488,9 @@ angular.module('starter')
 
                     var backView = $ionicHistory.backView();
                     if(backView.stateName.toLowerCase().indexOf('search') > -1){
-                        $ionicHistory.goBack(-2);
+                        $state.go(config.appSettings.defaultState);
+                        // This often doesn't work and the user should go to the inbox more anyway
+                        //$ionicHistory.goBack(-2);
                     } else {
                         $ionicHistory.goBack();
                     }
