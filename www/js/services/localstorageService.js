324b2a45e8c80c24d909fa5283c57b92a0bb78de
Mike P. Sinn <m@quantimodo.com>
2016-11-16 15:52:53 -0600

    Only show outcomes on predictor search list

--- www/js/services/localstorageService.js
+++ www/js/services/localstorageService.js
@@ -285,6 +285,9 @@ angular.module('starter')
                         filterPropertyValue = Number(filterPropertyValue);
                     }
                     filterPropertyName = key;
+                } else if (typeof value === "boolean" && key === "outcome"){
+                    filterPropertyValue = value;
+                    filterPropertyName = key;
                 }
             }, log);
 
