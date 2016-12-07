5365cc5f2f68c08d2479ce31a46f625d506e7aef
Mike P. Sinn <m@quantimodo.com>
2016-12-07 15:45:54 -0600

    Show menu on icons page

--- www/js/controllers/variableButtonIconsCtrl.js
+++ www/js/controllers/variableButtonIconsCtrl.js
@@ -1,15 +1,7 @@
 angular.module('starter')
 
-    .controller('VariableButtonIconsCtrl', function($scope, $rootScope) {
+    .controller('VariableButtonIconsCtrl', function($scope) {
 
         $scope.controller_name = "VariableButtonIconsCtrl";
-
-        $rootScope.hideNavigationMenu = true;
-
-        $scope.loadImages = function() {
-            for(var i = 0; i < 15; i++) {
-                $scope.images.push( );
-            }
-        };
 
     });
