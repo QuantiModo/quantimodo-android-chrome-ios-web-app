591a56211ba512a0b54aec7b5885c4c9c38a74b6
Mike Sinn <m@quantimodo.com>
2016-12-12 00:16:04 -0600

    More Android SDK install improvements

--- scripts/android_sdk_install.sh
+++ scripts/android_sdk_install.sh
@@ -5,14 +5,18 @@ apt-add-repository -y ppa:webupd8team/java
 apt-get update
 apt-get install -y oracle-java8-installer
 
-# download latest android sdk
-# http://developer.android.com/sdk/index.html#Other
-cd /opt
-wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
 
-tar -xvf android-sdk*-linux.tgz
-cd android-sdk-linux/tools
-./android update sdk --no-ui --filter platform,platform-tools
+if [ ! -d "/opt/android-sdk-linux" ];
+    then
+       # download latest android sdk
+        # http://developer.android.com/sdk/index.html#Other
+        cd /opt
+        wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
+
+        tar -xvf android-sdk*-linux.tgz
+        cd android-sdk-linux/tools
+        #./android update sdk --no-ui --filter platform,platform-tools
+fi
 
 # set path
 echo 'export PATH=$PATH:/opt/android-sdk-linux/platform-tools' >> /etc/profile.d/android.sh
@@ -40,6 +44,15 @@ expect {
     eof
 }
 '
+
+echo y | /opt/android-sdk-linux/tools/android update sdk --no-ui --all --filter tools
+echo y | /opt/android-sdk-linux/tools/android update sdk --no-ui --all --filter platform-tools
+echo y | /opt/android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-support
+echo y | /opt/android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-google-m2repository
+echo y | /opt/android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-m2repository
+
+if [ ! -d "/opt/android-sdk-linux/platforms/android-25" ]; then echo y | android update sdk --no-ui --all --filter "android-25"; fi
+if [ ! -d "/opt/android-sdk-linux/build-tools/25.0.1" ]; then echo y | android update sdk --no-ui --all --filter "build-tools-25.0.1"; fi
 
 sudo mkdir /opt/android-sdk-linux/licenses
 sudo cp ${IONIC_PATH}/android-licenses/* /opt/android-sdk-linux/licenses/
