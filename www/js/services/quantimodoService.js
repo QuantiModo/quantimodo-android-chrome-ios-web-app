8cd2dbd47ecba84dfebaec32dee7d48ee537867d
Mike Sinn <m@quantimodo.com>
2016-12-18 13:30:48 -0600

    Added variableService functions to quantimodoService

--- www/js/services/quantimodoService.js
+++ www/js/services/quantimodoService.js
@@ -1,6 +1,6 @@
 angular.module('starter')
     // quantimodoService API implementation
-    .factory('quantimodoService', function($http, $q, $rootScope, $ionicPopup, $state,
+    .factory('quantimodoService', function($http, $q, $rootScope, $ionicPopup, $state, $timeout,
                                     localStorageService) {
         var quantimodoService = {};
         $rootScope.offlineConnectionErrorShowing = false; // to prevent more than one popup
@@ -3524,6 +3524,7 @@ angular.module('starter')
             return deferred.promise;
         };
 
+        // ChartService
 
         var useLocalImages = function (correlationObjects) {
             for(var i = 0; i < correlationObjects.length; i++){
@@ -5059,6 +5060,285 @@ angular.module('starter')
                 }]
             };
         };
+
+
+        // VARIABLE SERVICE
+
+        // DOES NOT WORK PROPERLY
+        quantimodoService.searchUserVariablesDeferredFancy = function(variableSearchQuery, params){
+
+            if($rootScope.lastsearchUserVariablesDeferredPromise){
+                var message = 'Got new search request before last one completed';
+                console.debug(message);
+                $rootScope.lastsearchUserVariablesDeferredPromise.reject();
+                $rootScope.lastsearchUserVariablesDeferredPromise = null;
+            }
+
+            $rootScope.lastsearchUserVariablesDeferredPromise = $q.defer();
+
+            if(!variableSearchQuery){
+                variableSearchQuery = '*';
+            }
+
+            quantimodoService.searchUserVariablesFromApi(variableSearchQuery, params, function(variables){
+                if($rootScope.lastsearchUserVariablesDeferredPromise){
+                    $rootScope.lastsearchUserVariablesDeferredPromise.resolve(variables);
+                    $rootScope.lastsearchUserVariablesDeferredPromise = null;
+                } else {
+                    console.warn('Not resolving variables because no $rootScope.lastsearchUserVariablesDeferredPromise: ' +
+                        JSON.stringify(variables));
+                }
+            }, function(error){
+                console.error(JSON.stringify(error));
+                $rootScope.lastsearchUserVariablesDeferredPromise.reject(error);
+                $rootScope.lastsearchUserVariablesDeferredPromise = null;
+            });
+
+            return $rootScope.lastsearchUserVariablesDeferredPromise.promise;
+        };
+
+        // get user variables (without public)
+        quantimodoService.searchUserVariablesDeferred = function(variableSearchQuery, params){
+
+            var deferred = $q.defer();
+
+            if(!variableSearchQuery){
+                variableSearchQuery = '*';
+            }
+
+            quantimodoService.searchUserVariablesFromApi(variableSearchQuery, params, function(variables){
+                deferred.resolve(variables);
+            }, function(error){
+                console.error(JSON.stringify(error));
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        quantimodoService.getVariablesByNameDeferred = function(name, params){
+            var deferred = $q.defer();
+
+            // refresh always
+            quantimodoService.getVariablesByNameFromApi(name, params, function(variable){
+                deferred.resolve(variable);
+            }, function(error){
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        quantimodoService.getPublicVariablesByNameDeferred = function(name) {
+            var deferred = $q.defer();
+            quantimodoService.getPublicVariablesByNameFromApi(name, function(variable){
+                deferred.resolve(variable);
+            }, function(error){
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+
+        };
+
+        // post changes to user variable settings
+        quantimodoService.postUserVariableDeferred = function(userVariable) {
+
+            var deferred = $q.defer();
+            quantimodoService.postUserVariableToApi(userVariable, function(userVariable) {
+                deferred.resolve(userVariable);
+            }, function(error){
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        quantimodoService.resetUserVariableDeferred = function(variableId) {
+            var deferred = $q.defer();
+            var body = {variableId: variableId};
+            quantimodoService.resetUserVariable(body, function(userVariable) {
+                deferred.resolve(userVariable);
+            }, function(error){
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        quantimodoService.getVariableByIdDeferred = function(variableId){
+            var deferred = $q.defer();
+
+            // refresh always
+            quantimodoService.getVariableByIdFromApi(variableId, function(variable){
+                deferred.resolve(variable);
+            }, function(error){
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        quantimodoService.deleteAllMeasurementsForVariableDeferred = function(variableId) {
+            var deferred = $q.defer();
+            quantimodoService.deleteUserVariableMeasurements(variableId, function() {
+                // Delete user variable from local storage
+                localStorageService.deleteElementOfItemById('userVariables', variableId);
+                deferred.resolve();
+            }, function(error) {
+                if (typeof Bugsnag !== "undefined") { Bugsnag.notify(error, JSON.stringify(error), {}, "error"); }
+                console.error('Error deleting all measurements for variable: ', error);
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        quantimodoService.getUserVariablesDeferred = function(params){
+            var deferred = $q.defer();
+            var userVariables = localStorageService.getElementsFromItemWithRequestParams(
+                'userVariables', params);
+
+            if(userVariables && userVariables.length > 0){
+                deferred.resolve(userVariables);
+                return deferred.promise;
+            }
+
+            if(localStorageService.getItemSync('userVariables') === "[]"){
+                deferred.resolve([]);
+                return deferred.promise;
+            }
+
+            userVariables = JSON.parse(localStorageService.getItemSync('userVariables'));
+            if(userVariables && userVariables.length && typeof userVariables[0].manualTracking !== "undefined"){
+                console.debug("We already have userVariables that didn't match filters so no need to refresh them");
+                deferred.resolve([]);
+                return deferred.promise;
+            }
+
+            quantimodoService.refreshUserVariables().then(function () {
+                userVariables = localStorageService.getElementsFromItemWithRequestParams(
+                    'userVariables', params);
+                deferred.resolve(userVariables);
+            }, function (error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        quantimodoService.refreshUserVariables = function(){
+            var deferred = $q.defer();
+            if($rootScope.syncingUserVariables){
+                console.warn('Already called refreshUserVariables within last 10 seconds!  Rejecting promise!');
+                deferred.reject('Already called refreshUserVariables within last 10 seconds!  Rejecting promise!');
+                return deferred.promise;
+            }
+
+            if(!$rootScope.syncingUserVariables){
+                $rootScope.syncingUserVariables = true;
+                console.debug('Setting refreshUserVariables timeout');
+                $timeout(function() {
+                    // Set to false after 10 seconds because it seems to get stuck on true sometimes for some reason
+                    $rootScope.syncingUserVariables = false;
+                }, 10000);
+
+                var parameters = {
+                    limit: 200,
+                    sort: "-latestMeasurementTime"
+                };
+
+                quantimodoService.getUserVariablesFromApi(parameters, function(userVariables){
+                    localStorageService.setItem('userVariables', JSON.stringify(userVariables))
+                        .then(function () {
+                            $rootScope.$broadcast('populateUserVariables');
+                            $rootScope.syncingUserVariables = false;
+                        });
+                    deferred.resolve(userVariables);
+                }, function(error){
+                    $rootScope.syncingUserVariables = false;
+                    deferred.reject(error);
+                });
+
+                return deferred.promise;
+            }
+        };
+
+        quantimodoService.getCommonVariablesDeferred = function(params){
+            var deferred = $q.defer();
+            var commonVariables = localStorageService.getElementsFromItemWithRequestParams(
+                'commonVariables', params);
+
+            if(commonVariables && commonVariables.length && typeof commonVariables[0].manualTracking !== "undefined"){
+                deferred.resolve(commonVariables);
+                return deferred.promise;
+            }
+
+            commonVariables = JSON.parse(localStorageService.getItemSync('commonVariables'));
+            if(commonVariables && commonVariables.length && typeof commonVariables[0].manualTracking !== "undefined"){
+                console.debug("We already have commonVariables that didn't match filters so no need to refresh them");
+                deferred.resolve([]);
+                return deferred.promise;
+            }
+
+            quantimodoService.refreshCommonVariables().then(function () {
+                commonVariables = localStorageService.getElementsFromItemWithRequestParams(
+                    'commonVariables', params);
+                deferred.resolve(commonVariables);
+            }, function (error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        quantimodoService.refreshCommonVariables = function(){
+            var deferred = $q.defer();
+            if($rootScope.syncingCommonVariables){
+                console.warn('Already called refreshCommonVariables within last 10 seconds!  Rejecting promise!');
+                deferred.reject('Already called refreshCommonVariables within last 10 seconds!  Rejecting promise!');
+                return deferred.promise;
+            }
+
+            if(!$rootScope.syncingCommonVariables){
+                $rootScope.syncingCommonVariables = true;
+                console.debug('Setting refreshCommonVariables timeout');
+                $timeout(function() {
+                    // Set to false after 10 seconds because it seems to get stuck on true sometimes for some reason
+                    $rootScope.syncingCommonVariables = false;
+                }, 10000);
+
+                var successHandler = function(commonVariables) {
+                    localStorageService.setItem('commonVariables', JSON.stringify(commonVariables)).then(function () {
+                        $rootScope.$broadcast('populateCommonVariables');
+                    });
+                    $rootScope.syncingCommonVariables = false;
+                    deferred.resolve(commonVariables);
+                };
+
+                var errorHandler = function(error) {
+                    $rootScope.syncingCommonVariables = false;
+                    if (typeof Bugsnag !== "undefined") { Bugsnag.notify("ERROR: " + JSON.stringify(error), JSON.stringify(error), {}, "error"); } console.error(error);
+                    deferred.reject(error);
+                };
+
+                var parameters = {
+                    limit: 200,
+                    sort: "-numberOfUserVariables",
+                    numberOfUserVariables: "(gt)3"
+                };
+
+
+                quantimodoService.get('api/v1/public/variables',
+                    ['category', 'includePublic', 'numberOfUserVariables'],
+                    parameters,
+                    successHandler,
+                    errorHandler);
+
+                return deferred.promise;
+            }
+        };
+
 
         return quantimodoService;
     });
