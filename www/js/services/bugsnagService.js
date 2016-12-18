8ace21b41f9697a5413cd1549b20a4fda6b06237
Mike Sinn <m@quantimodo.com>
2016-12-18 12:39:32 -0600

    Removed bugsnagService

--- www/js/services/bugsnagService.js
+++
@@ -1,7 +0,0 @@
-angular.module('starter')
-    // Measurement Service
-    .factory('quantimodoService', function(quantimodoService, $q, $rootScope) {
-
-
-        return quantimodoService;
-    });
