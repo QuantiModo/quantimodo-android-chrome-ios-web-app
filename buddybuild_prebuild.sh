51653cc212285a5b910f62157f9183055278cec4
Mike Sinn <m@quantimodo.com>
2016-12-21 16:04:46 -0600

    Build logging

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 sudo npm install -g gulp bower ionic cordova
+echo "Running sudo npm install -g gulp bower ionic cordova"
 sudo brew install imagemagick
+echo "Running sudo brew install imagemagick"
 npm install
+echo "Running npm install"
 gulp prepareQuantiModoIos
+echo "Running gulp prepareQuantiModoIos"
 gulp buildAndReleaseIosApp
+echo "Running gulp buildAndReleaseIosApp"
