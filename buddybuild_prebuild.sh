9650d85ed6ba49801eafd021a5bbbcbd5dcc8873
Mike P. Sinn <m@quantimodo.com>
2016-12-21 21:32:38 -0600

    chmod -R a+x ../../hooks

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -3,9 +3,9 @@
 echo "Current folder is $PWD"
 echo "Folder contents are:"
 ls
-chmod -R a+x hooks
-chmod -R a+x package-hooks
-chmod -R a+x scripts
+chmod -R a+x ../../hooks
+chmod -R a+x ../../package-hooks
+chmod -R a+x ../../scripts
 
 sudo npm install -g gulp bower ionic cordova
 echo "Running sudo npm install -g gulp bower ionic cordova"
