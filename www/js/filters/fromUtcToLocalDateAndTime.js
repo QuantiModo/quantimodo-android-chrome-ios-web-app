579070fc947f40e7839771c648f3e08abcb674b7
Mike P. Sinn <m@quantimodo.com>
2017-01-16 20:28:03 -0600

    Inbox notification styling

--- www/js/filters/fromUtcToLocalDateAndTime.js
+++ www/js/filters/fromUtcToLocalDateAndTime.js
@@ -1,5 +1,20 @@
 angular.module('starter')
 	// returns string for time
+	.filter('fromUtcToLocalDate', function(){
+		var localDateAndTime;
+		return function(epochTime){
+			if(epochTime){
+				if(typeof epochTime === "number") {
+					localDateAndTime = moment(epochTime * 1000).format(" dddd, MMM Do");
+					return localDateAndTime;
+				}
+				localDateAndTime = moment.utc(epochTime).local().format(" dddd, MMMM Do");
+				return localDateAndTime;
+			} else {
+				return "";
+			}
+		};
+	})
 	.filter('fromUtcToLocalDateAndTime', function(){
 		var localDateAndTime;
 	    return function(epochTime){
