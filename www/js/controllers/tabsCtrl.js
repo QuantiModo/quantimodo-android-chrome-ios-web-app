e2b2207f7ac7890c518680f5f4798d8c29e86331
Mike P. Sinn <m@quantimodo.com>
2017-01-18 11:24:24 -0600

    Dynamic tabs title

--- www/js/controllers/tabsCtrl.js
+++ www/js/controllers/tabsCtrl.js
@@ -2,7 +2,7 @@ angular.module('starter')
 .controller('TabsCtrl', function($scope, $state) {
 
     $scope.$on('$ionicView.beforeEnter', function(e) { console.debug("Entering state " + $state.current.name);
-
+        $scope.tabsTitle = "Tabs Title";
     });
 
 });
