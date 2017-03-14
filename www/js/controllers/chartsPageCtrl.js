22ddbe2c6ad929d68d632e581dd64350ae1c90be
Mike P. Sinn <m@quantimodo.com>
2017-03-13 21:02:45 -0500

    Show nav menu on charts page

--- www/js/controllers/chartsPageCtrl.js
+++ www/js/controllers/chartsPageCtrl.js
@@ -1,7 +1,4 @@
-angular.module('starter')
-
-    // Controls the Track Page of the App
-    .controller('ChartsPageCtrl', function($scope, $q, $state, $timeout, $rootScope, $ionicLoading,  $ionicActionSheet,
+angular.module('starter').controller('ChartsPageCtrl', function($scope, $q, $state, $timeout, $rootScope, $ionicLoading,  $ionicActionSheet,
                                              $stateParams, quantimodoService, clipboard) {
         $scope.controller_name = "ChartsPageCtrl";
         $scope.addReminderButtonText = "Add Reminder";
@@ -19,15 +16,11 @@ angular.module('starter')
             dailyHistoryOffset: 0
         };
         var maximumMeasurements = 2000;
-
         $scope.$on('$ionicView.enter', function(e) { console.debug("Entering state " + $state.current.name);
-            if($rootScope.urlParameters.variableName){
-                $stateParams.variableName = $rootScope.urlParameters.variableName;
-            }
-
+            if($rootScope.urlParameters.variableName){$stateParams.variableName = $rootScope.urlParameters.variableName;}
+            $rootScope.hideNavigationMenu = false;
             $scope.stopGettingMeasurements = false;
             $ionicLoading.hide();
-            //$scope.showLoader('Fetching measurements');
             console.debug("variablePageCtrl: init");
             if($stateParams.variableObject){
                 $rootScope.variableObject = $stateParams.variableObject;
@@ -38,33 +31,16 @@ angular.module('starter')
                 if(!$rootScope.variableObject || $rootScope.variableObject.name !== $stateParams.variableName){
                     getStatisticsForVariable($stateParams.variableName);
                 }
-            } else {
-                console.error("ERROR: chartsPageCtrl.init No variable name provided!");
-                return;
-            }
-
+            } else { $scope.goBack();}
             $ionicLoading.hide();
-
             if($rootScope.variableObject.name){
                 $rootScope.variableName = $rootScope.variableObject.name;
-                var params = {
-                    sort: "-startTimeEpoch",
-                    variableName: $rootScope.variableObject.name,
-                    limit: 2000,
-                    offset: 0
-                };
-                if($rootScope.urlParameters.userId){
-                    params.userId = $rootScope.urlParameters.userId;
-                }
+                var params = {sort: "-startTimeEpoch", variableName: $rootScope.variableObject.name, limit: maximumMeasurements, offset: 0};
+                if($rootScope.urlParameters.userId){params.userId = $rootScope.urlParameters.userId;}
                 getDailyHistoryForVariable(params);
                 getHistoryForVariable(params);
-            } else {
-                console.error($state.current.name + ' ERROR: $rootScope.variableObject.name not defined! $rootScope.variableObject: ' +
-                    JSON.stringify($rootScope.variableObject));
-            }
-
+            } else {console.error($state.current.name + ' ERROR: $rootScope.variableObject.name not defined! $rootScope.variableObject: ' + JSON.stringify($rootScope.variableObject));}
             $rootScope.showActionSheetMenu = function() {
-
                 console.debug("variablePageCtrl.showActionSheetMenu:  $rootScope.variableObject: ", $rootScope.variableObject);
                 var hideSheet = $ionicActionSheet.show({
                     buttons: [
@@ -78,34 +54,16 @@ angular.module('starter')
                     ],
                     destructiveText: '<i class="icon ion-trash-a"></i>Delete All',
                     cancelText: '<i class="icon ion-ios-close"></i>Cancel',
-                    cancel: function() {
-                        console.debug('CANCELLED');
-                    },
+                    cancel: function() {console.debug('CANCELLED');},
                     buttonClicked: function(index) {
                         console.debug('BUTTON CLICKED', index);
-                        if(index === 0){
-                            $scope.addToFavoritesUsingVariableObject($rootScope.variableObject);
-                        }
-                        if(index === 1){
-                            $scope.goToAddMeasurementForVariableObject($rootScope.variableObject);
-                        }
-                        if(index === 2){
-                            $scope.goToAddReminderForVariableObject($rootScope.variableObject);
-                        }
-                        if(index === 3) {
-                            $scope.goToHistoryForVariableObject($rootScope.variableObject);
-                        }
-                        if (index === 4) {
-                            $state.go('app.variableSettings', {variableObject: $rootScope.variableObject});
-                        }
-                        if (index === 5) {
-                            $scope.addTag($rootScope.variableObject);
-                        }
-                        if(index === 6) {
-                            console.debug('variableSettingsCtrl going to history' + JSON.stringify($rootScope.variableObject));
-                            $scope.tagAnotherVariable($rootScope.variableObject);
-                        }
-
+                        if(index === 0){$scope.addToFavoritesUsingVariableObject($rootScope.variableObject);}
+                        if(index === 1){$scope.goToAddMeasurementForVariableObject($rootScope.variableObject);}
+                        if(index === 2){$scope.goToAddReminderForVariableObject($rootScope.variableObject);}
+                        if(index === 3) {$scope.goToHistoryForVariableObject($rootScope.variableObject);}
+                        if (index === 4) {$state.go('app.variableSettings', {variableObject: $rootScope.variableObject});}
+                        if (index === 5) {$scope.addTag($rootScope.variableObject);}
+                        if(index === 6) {$scope.tagAnotherVariable($rootScope.variableObject);}
                         return true;
                     },
                     destructiveButtonClicked: function() {
@@ -113,123 +71,58 @@ angular.module('starter')
                         return true;
                     }
                 });
-
-                console.debug('Setting hideSheet timeout');
-                $timeout(function() {
-                    hideSheet();
-                }, 20000);
-
+                $timeout(function() {hideSheet();}, 20000);
             };
         });
-
         $scope.$on('$ionicView.beforeLeave', function(){
             console.debug('Leaving so setting $scope.stopGettingMeasurements to true');
             $scope.stopGettingMeasurements = true;
         });
-
         if (!clipboard.supported) {
             console.log('Sorry, copy to clipboard is not supported');
             $scope.hideClipboardButton = true;
         }
-
         $scope.addNewReminderButtonClick = function() {
             console.debug("addNewReminderButtonClick");
-            $state.go('app.reminderAdd', {
-                variableObject: $rootScope.variableObject,
-                fromState: $state.current.name
-            });
+            $state.go('app.reminderAdd', {variableObject: $rootScope.variableObject, fromState: $state.current.name});
         };
-
-        $scope.recordMeasurementButtonClick = function() {
-            $state.go('app.measurementAdd', {
-                variableObject: $rootScope.variableObject,
-                fromState: $state.current.name
-            });
-        };
-
-        $scope.editSettingsButtonClick = function() {
-            $state.go('app.variableSettings', {variableObject: $rootScope.variableObject});
-        };
-
-
+        $scope.recordMeasurementButtonClick = function() {$state.go('app.measurementAdd', {variableObject: $rootScope.variableObject, fromState: $state.current.name});};
+        $scope.editSettingsButtonClick = function() {$state.go('app.variableSettings', {variableObject: $rootScope.variableObject});};
         var updateDailyCharts = function(){
-
             if ($scope.state.dailyHistory.length > 0) {
-                // FIXME Eventually update fromDate and toDate so calendar can determine domain
-                /*var fromDate = parseInt(quantimodoService.getLocalStorageItemAsString('fromDate'));
-                 var toDate = parseInt(quantimodoService.getLocalStorageItemAsString('toDate'));
-                 if (!fromDate) {
-                 fromDate = 0;
-                 }
-                 if (!toDate) {
-                 toDate = Date.now();
-                 }*/
                 if($rootScope.variableObject.fillingValue !== null && $rootScope.variableObject.fillingValue !== -1){
-                    $scope.distributionChartConfig =
-                        quantimodoService.processDataAndConfigureDistributionChart($scope.state.dailyHistory, $rootScope.variableObject);
+                    $scope.distributionChartConfig = quantimodoService.processDataAndConfigureDistributionChart($scope.state.dailyHistory, $rootScope.variableObject);
                 }
                 $scope.lineChartConfig = quantimodoService.processDataAndConfigureLineChart($scope.state.dailyHistory, $rootScope.variableObject);
-                $scope.weekdayChartConfig =
-                    quantimodoService.processDataAndConfigureWeekdayChart($scope.state.dailyHistory, $rootScope.variableObject);
-                $scope.monthlyChartConfig =
-                    quantimodoService.processDataAndConfigureMonthlyChart($scope.state.dailyHistory, $rootScope.variableObject);
+                $scope.weekdayChartConfig = quantimodoService.processDataAndConfigureWeekdayChart($scope.state.dailyHistory, $rootScope.variableObject);
+                $scope.monthlyChartConfig = quantimodoService.processDataAndConfigureMonthlyChart($scope.state.dailyHistory, $rootScope.variableObject);
                 $scope.highchartsReflow();
             }
         };
-
         var updateCharts = function(){
-
             if ($scope.state.history.length > 0) {
-                // FIXME Eventually update fromDate and toDate so calendar can determine domain
-                /*var fromDate = parseInt(quantimodoService.getLocalStorageItemAsString('fromDate'));
-                var toDate = parseInt(quantimodoService.getLocalStorageItemAsString('toDate'));
-                if (!fromDate) {
-                    fromDate = 0;
-                }
-                if (!toDate) {
-                    toDate = Date.now();
-                }*/
                 if($rootScope.variableObject.fillingValue === null || $rootScope.variableObject.fillingValue === -1){
-                    $scope.distributionChartConfig =
-                        quantimodoService.processDataAndConfigureDistributionChart($scope.state.history, $rootScope.variableObject);
+                    $scope.distributionChartConfig = quantimodoService.processDataAndConfigureDistributionChart($scope.state.history, $rootScope.variableObject);
                 }
-                $scope.hourlyChartConfig =
-                    quantimodoService.processDataAndConfigureHourlyChart($scope.state.history, $rootScope.variableObject);
+                $scope.hourlyChartConfig = quantimodoService.processDataAndConfigureHourlyChart($scope.state.history, $rootScope.variableObject);
                 $scope.highchartsReflow();
             }
         };
-
         var getHistoryForVariable = function(params){
-
-            if($scope.stopGettingMeasurements){
-                console.debug('Stopping getting of measurements');
-                return;
-            }
-
+            if($scope.stopGettingMeasurements){return;}
             if(!params.variableName){
                 console.error("ERROR: params.variableName not provided to getHistoryForVariable");
                 console.error($state.current.name + " params: " + JSON.stringify(params));
                 console.error($state.current.name + " $rootScope.variableObject: " + JSON.stringify($rootScope.variableObject));
                 return;
             }
-
-            if($rootScope.urlParameters.doNotProcess){
-                params.doNotProcess = true;
-            }
-            //$scope.showLoader('Fetching measurements');
+            if($rootScope.urlParameters.doNotProcess){params.doNotProcess = true;}
             $scope.state.loadingHistory = true;
             quantimodoService.getMeasurementsFromApi(params, function(history){
                 $scope.state.history = $scope.state.history.concat(history);
-
                 if(params.limit > 0 && history.length > 0 && $scope.state.history.length < maximumMeasurements){
                     $scope.state.offset = $scope.state.offset + 200;
-                    params = {
-                        offset: $scope.state.offset,
-                        sort: "-startTimeEpoch",
-                        variableName: $rootScope.variableObject.name,
-                        limit: 0
-                    };
-                    if($rootScope.urlParameters.userId){params.userId = $rootScope.urlParameters.userId;}
+                    params.offset = $scope.state.offset;
                     updateCharts();
                     getHistoryForVariable(params);
                 } else {
@@ -246,45 +139,28 @@ angular.module('starter')
                 $scope.state.history = $scope.state.history.concat(history);
             });
         };
-
         var getDailyHistoryForVariable = function(params){
-            if($scope.stopGettingMeasurements){
-                console.debug('Stopping getting of measurements');
-                return;
-            }
+            if($scope.stopGettingMeasurements){return;}
             if(!params.variableName){
                 console.error("ERROR: params.variableName not provided to getHistoryForVariable");
                 console.error("params: " + JSON.stringify(params));
                 console.error("$rootScope.variableObject: " + JSON.stringify($rootScope.variableObject));
                 return;
             }
-            //$scope.showLoader('Fetching measurements');
             $scope.state.loadingDailyHistory = true;
             quantimodoService.getMeasurementsDailyFromApiDeferred(params).then(function(dailyHistory){
                 $scope.state.dailyHistory = $scope.state.dailyHistory.concat(dailyHistory);
                 if(params.limit > 0 && dailyHistory.length > 0 && $scope.state.dailyHistory.length < maximumMeasurements){
                     $scope.state.dailyHistoryOffset = $scope.state.dailyHistoryOffset + 200;
-                    params = {
-                        offset: $scope.state.dailyHistoryOffset,
-                        sort: "-startTimeEpoch",
-                        variableName: $rootScope.variableObject.name,
-                        limit: 0
-                    };
-                    if($rootScope.urlParameters.userId){
-                        params.userId = $rootScope.urlParameters.userId;
-                    }
+                    params.offset = $scope.state.dailyHistoryOffset;
                     updateDailyCharts();
                     getDailyHistoryForVariable(params);
                 } else {
                     $scope.state.loadingDailyHistory = false;
-                    if ($scope.state.dailyHistory.length > 0) {
-                        updateDailyCharts();
-                    }
+                    if ($scope.state.dailyHistory.length > 0) {updateDailyCharts();}
                 }
             }, function(error){
-                if (typeof Bugsnag !== "undefined") {
-                    Bugsnag.notify(error, JSON.stringify(error), {}, "error");
-                }
+                if (typeof Bugsnag !== "undefined") {Bugsnag.notify(error, JSON.stringify(error), {}, "error");}
                 console.error($state.current.name + ' error getting dailyHistory measurements: ' + JSON.stringify(error));
                 $scope.hideLoader();
                 $scope.state.loadingDailyHistory = false;
@@ -293,26 +169,17 @@ angular.module('starter')
                 $scope.state.dailyHistory = $scope.state.dailyHistory.concat(history);
             });
         };
-
         var refreshUserVariable = function (variableName) {
-            quantimodoService.refreshUserVariableDeferred(variableName).then(function (variableObject) {
-                $rootScope.variableObject = variableObject;
-            });
+            quantimodoService.refreshUserVariableDeferred(variableName).then(function (variableObject) {$rootScope.variableObject = variableObject;});
         };
-
         var getStatisticsForVariable = function (variableName) {
-            $rootScope.variableObject = {
-                name:  variableName
-            };
+            $rootScope.variableObject = {name:  variableName};
             $rootScope.variableName = variableName;
             var params = {};
-            if($rootScope.urlParameters.userId){
-                params.userId = $rootScope.urlParameters.userId;
-            }
+            if($rootScope.urlParameters.userId){params.userId = $rootScope.urlParameters.userId;}
             quantimodoService.getUserVariableByNameDeferred(variableName, params).then(function(variableObject){
                 $rootScope.variableObject = variableObject;
                 refreshUserVariable(variableName);
             });
         };
-
     });
