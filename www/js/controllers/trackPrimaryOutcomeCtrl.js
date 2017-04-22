4e268bde2e823cd90a3129fdd7d21d04c61b2bfd
Mike P. Sinn <m@quantimodo.com>
2017-04-21 19:33:31 -0500

    Always sync primary outcome measurements

--- www/js/controllers/trackPrimaryOutcomeCtrl.js
+++ www/js/controllers/trackPrimaryOutcomeCtrl.js
@@ -14,15 +14,13 @@ angular.module('starter').controller('TrackPrimaryOutcomeCtrl', function($scope,
         updateCharts();
         $scope.showRatingFaces = true;
         $scope.timeRemaining = false;
-        if($rootScope.user || quantimodoService.getUrlParameter('accessToken')){
-            $scope.showLoader(syncDisplayText);
-            console.debug($state.current.name + ' going to syncPrimaryOutcomeVariableMeasurements');
-            quantimodoService.syncPrimaryOutcomeVariableMeasurements().then(function(){
-                $scope.hideLoader();
-                updateCharts();
-                $ionicLoading.hide();
-            });
-        } else {console.debug($state.current.name + ' has no user or access token so we cannot syncPrimaryOutcomeVariableMeasurements');}
+        $scope.showLoader(syncDisplayText);
+        console.debug($state.current.name + ' going to syncPrimaryOutcomeVariableMeasurements');
+        quantimodoService.syncPrimaryOutcomeVariableMeasurements().then(function(){
+            $scope.hideLoader();
+            updateCharts();
+            $ionicLoading.hide();
+        });
     });
     $scope.storeRatingLocalAndServerAndUpdateCharts = function (numericRatingValue) {
         $scope.timeRemaining = true;
