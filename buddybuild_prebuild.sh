faf304de75718cb6f3308fbe15776babab0e6f51
Mike P. Sinn <m@quantimodo.com>
2017-05-07 11:52:41 -0500

    Try running gulp getCommonVariables in BuddyBuild pre-build step

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -7,5 +7,7 @@ echo "PREPARE_IOS_APP env is ${PREPARE_IOS_APP}"
 
 echo "LOWERCASE_APP_NAME is ${LOWERCASE_APP_NAME}"
 
+gulp getCommonVariables
+
 #echo "cd ../.. && gulp prepareQuantiModoIos && cd platforms/ios"
 #cd ../.. && gulp prepareQuantiModoIos && cd platforms/ios
