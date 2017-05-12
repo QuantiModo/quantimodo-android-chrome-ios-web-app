aed460a67d065566f69d356bab9311d5b11adf58
Mike Sinn <m@quantimodo.com>
2017-05-12 15:58:44 -0500

    Output SHA1 after BuddyBuild

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -14,3 +14,13 @@ cd www/configs && find .
 
 echo "www/data FILE LIST:"
 cd .. && cd data && find .
+
+if [ -z ${BUDDYBUILD_SCHEME} ];
+    then
+        keytool -list -printcert -jarfile ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-armv7-release.apk | grep -Po "(?<=SHA1:) .*" |  xxd -r -p | openssl base64
+        keytool -list -printcert -jarfile ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-x86-release.apk | grep -Po "(?<=SHA1:) .*" |  xxd -r -p | openssl base64
+        keytool -list -printcert -jarfile ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-armv7-debug.apk | grep -Po "(?<=SHA1:) .*" |  xxd -r -p | openssl base64 || true
+    else
+        echo "BUILDING IOS APP because BUDDYBUILD_SCHEME env is ${BUDDYBUILD_SCHEME}"
+        echo "NOT BUILDING ANDROID APP because BUDDYBUILD_SCHEME env is set"
+fi
