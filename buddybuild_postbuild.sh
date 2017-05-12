38ddea5c45b78741c7b89614d15c5c83db283bd1
Mike Sinn <m@quantimodo.com>
2017-05-12 16:00:46 -0500

    echo "android-armv7-release.apk"

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -17,8 +17,11 @@ cd .. && cd data && find .
 
 if [ -z ${BUDDYBUILD_SCHEME} ];
     then
+        echo "android-armv7-release.apk"
         keytool -list -printcert -jarfile ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-armv7-release.apk | grep -Po "(?<=SHA1:) .*" |  xxd -r -p | openssl base64
+        echo "android-x86-release.apk"
         keytool -list -printcert -jarfile ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-x86-release.apk | grep -Po "(?<=SHA1:) .*" |  xxd -r -p | openssl base64
+        echo "android-armv7-debug.apk"
         keytool -list -printcert -jarfile ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-armv7-debug.apk | grep -Po "(?<=SHA1:) .*" |  xxd -r -p | openssl base64 || true
     else
         echo "BUILDING IOS APP because BUDDYBUILD_SCHEME env is ${BUDDYBUILD_SCHEME}"
