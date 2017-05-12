9edf937059ab501a5208983f459351ea05e9979b
Mike Sinn <m@quantimodo.com>
2017-05-12 16:50:03 -0500

    Less verbose logging

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -19,15 +19,16 @@ if [ -z ${BUDDYBUILD_SCHEME} ];
     then
         echo "android-armv7-release.apk"
         keytool -list -printcert -jarfile ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-armv7-release.apk | grep -Po "(?<=SHA1:) .*" |  xxd -r -p | openssl base64
-        unzip ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-armv7-release.apk
+        #unzip android-armv7-release.apk >/dev/null
+        unzip ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-armv7-release.apk >/dev/null
         keytool -printcert -file META-INF/CERT.RSA
         echo "android-x86-release.apk"
         keytool -list -printcert -jarfile ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-x86-release.apk | grep -Po "(?<=SHA1:) .*" |  xxd -r -p | openssl base64
-        unzip ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-x86-release.apk
+        unzip ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-x86-release.apk >/dev/null
         keytool -printcert -file META-INF/CERT.RSA
         echo "android-armv7-debug.apk"
         keytool -list -printcert -jarfile ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-armv7-debug.apk | grep -Po "(?<=SHA1:) .*" |  xxd -r -p | openssl base64 || true
-        unzip ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-armv7-debug.apk
+        unzip ${BUDDYBUILD_WORKSPACE}/platforms/android/build/outputs/apk/android-armv7-debug.apk >/dev/null
         keytool -printcert -file META-INF/CERT.RSA
     else
         echo "BUILDING IOS APP because BUDDYBUILD_SCHEME env is ${BUDDYBUILD_SCHEME}"
