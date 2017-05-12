b6c12c9695ebbf0ed1d2937a1c122def1fd4cae1
Mike Sinn <m@quantimodo.com>
2017-05-12 16:18:02 -0500

    Use https when getting weather location

--- www/js/services/quantimodoService.js
+++ www/js/services/quantimodoService.js
@@ -1613,8 +1613,8 @@ angular.module('starter').factory('quantimodoService', function($http, $q, $root
     };
     quantimodoService.connectConnectorWithParamsDeferred = function(params, lowercaseConnectorName){
         var deferred = $q.defer();
-        if(lowercaseConnectorName.indexOf('weather')> -1){
-            $http.get('//freegeoip.net/json/').success(function(data) {
+        if(lowercaseConnectorName.indexOf('weather')> -1 && !params.location){
+            $http.get('https://freegeoip.net/json/').success(function(data) {
                 console.log(JSON.stringify(data, null, 2));
                 quantimodoService.connectConnectorWithParamsToApi({location: data.ip}, lowercaseConnectorName, function(){quantimodoService.refreshConnectors();}, function(error){deferred.reject(error);});
             });
