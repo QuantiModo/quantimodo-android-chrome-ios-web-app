b672946b6a4a7f8d241cc2741f2f9e2bdd07e235
Mike P. Sinn <m@quantimodo.com>
2017-06-03 14:18:47 -0500

    weShouldUseOAuthLogin

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -91,14 +91,21 @@ angular.module('starter').controller('LoginCtrl', function($scope, $state, $root
         var register = true;
         $scope.login(register);
     };
+    function weShouldUseOAuthLogin() {
+        if(quantimodoService.getClientId() === "oAuthDisabled"){return false;}
+        if(quantimodoService.getClientId() === "staging"){return false;}
+        if(quantimodoService.getClientId() === "app"){return false;}
+        if(quantimodoService.getClientId() === "local"){return false;}
+        if(quantimodoService.getClientId() === "local"){return false;}
+        return window.private_keys;
+    }
     var browserLogin = function(register) {
         console.debug("Browser Login");
         if (window.private_keys && window.private_keys.username) {
             quantimodoService.refreshUser().then(function () {$state.go(config.appSettings.appDesign.defaultState);});
-        } else if (quantimodoService.getClientId() !== 'oAuthDisabled' && window.private_keys) {
+        } else if (weShouldUseOAuthLogin()) {
             // Using timeout to avoid "$apply already in progress" error caused by window.open
-            if($scope.$root.$$phase) { $timeout(function() { quantimodoService.oAuthBrowserLogin(register); },0,false);
-            } else { quantimodoService.oAuthBrowserLogin(register); }
+            if($scope.$root.$$phase) { $timeout(function() { quantimodoService.oAuthBrowserLogin(register); },0,false);} else { quantimodoService.oAuthBrowserLogin(register); }
         } else {
             quantimodoService.sendToNonOAuthBrowserLoginUrl(register);
         }
