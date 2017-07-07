9321547b8586896d26131e703a2cc4cf90757f17
Mike P. Sinn <m@quantimodo.com>
2017-07-07 14:57:04 -0500

    Faster develop builds

--- scripts/circleci_build_android_apps_and_chrome_extensions.sh
+++ scripts/circleci_build_android_apps_and_chrome_extensions.sh
@@ -3,5 +3,5 @@
 gulp buildAllChromeExtensionsAndAndroidApps
 #gulp buildQuantiModoAndroid  # Did not solve "Your build has exceeded the memory limit of 4G on 1 container"
 if [ ! -f platforms/android/build/outputs/apk/android-armv7-release.apk ]; then exit 1; fi
-if [ -f platforms/android/build/outputs/apk/android-armv7-release.apk ]; then echo "Succesfully built Android app" ; fi
+if [ -f platforms/android/build/outputs/apk/android-armv7-release.apk ]; then echo "Successfully built Android app" ; fi
 cp -r build/*.zip ${CIRCLE_ARTIFACTS}
