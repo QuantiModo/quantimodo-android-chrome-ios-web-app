53e79fcd2e0a6060016aace1f8d1f21311c79fd3
Mike P. Sinn <m@quantimodo.com>
2017-07-13 00:30:38 -0500

    Dynamically update favicon

--- www/js/services/quantimodoService.js
+++ www/js/services/quantimodoService.js
@@ -6694,6 +6694,20 @@ angular.module('starter').factory('quantimodoService', function($http, $q, $root
             });
         }
         if(getUrlParameter('finish_url')){$rootScope.finishUrl = getUrlParameter('finish_url', null, true);}
+        //noinspection JSAnnotator
+        document.head || (document.head = document.getElementsByTagName('head')[0]);
+        function changeFavicon(src) {
+            var link = document.createElement('link'),
+                oldLink = document.getElementById('dynamic-favicon');
+            link.id = 'dynamic-favicon';
+            link.rel = 'shortcut icon';
+            link.href = src;
+            if (oldLink) {
+                document.head.removeChild(oldLink);
+            }
+            document.head.appendChild(link);
+        }
+        changeFavicon('img/icons/icon_16.png')
     };
     quantimodoService.getUserFromLocalStorageOrRefreshIfNecessary = function(){
         logDebugMessage("getUserFromLocalStorageOrRefreshIfNecessary");
