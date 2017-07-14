9059f61cdf2ee3779c3e33d8ea94df653a063d06
Mike P. Sinn <m@quantimodo.com>
2017-07-14 12:12:40 -0500

    I think adding appSettings to the chrome manifest breaks installation

--- gulpfile.js
+++ gulpfile.js
@@ -632,7 +632,7 @@ gulp.task('createChromeExtensionManifest', function () {
     chromeExtensionManifest.description = appSettings.appDescription;
     chromeExtensionManifest.version = versionNumbers.ionicApp;
     chromeExtensionManifest.permissions.push("https://*.quantimo.do/*");
-    //chromeExtensionManifest.appSettings = appSettings;
+    //chromeExtensionManifest.appSettings = appSettings; // I think adding appSettings to the chrome manifest breaks installation
     chromeExtensionManifest = JSON.stringify(chromeExtensionManifest, null, 2);
     var chromeManifestPath = chromeExtensionBuildPath + '/manifest.json';
     infoLog("Creating chrome manifest at " + chromeManifestPath);
