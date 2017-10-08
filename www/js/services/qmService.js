a97d460ae58b88866a239f04724e47d692fbfcb1
Mike P. Sinn <m@quantimodo.com>
2017-10-08 11:50:43 -0500

    qmService.overApps

--- www/js/services/qmService.js
+++ www/js/services/qmService.js
@@ -7809,5 +7809,28 @@ angular.module('starter').factory('qmService', function($http, $q, $rootScope, $
     //         });
     //     });
     // };
+    qmService.overApps = function() {
+        $ionicPlatform.ready(function() {
+            window.overApps.checkPermission(function(msg){
+                console.log(msg);
+            });
+            var options = {
+                path: "index.html",          // file path to display as view content.
+                hasHead: true,              // display over app head image which open the view up on click.
+                dragToSide: false,          // enable auto move of head to screen side after dragging stop.
+                enableBackBtn: false,       // enable hardware back button to close view.
+                enableCloseBtn: true,      //  whether to show native close btn or to hide it.
+                verticalPosition: "top",    // set vertical alignment of view.
+                horizontalPosition: "left"  // set horizontal alignment of view.
+        };
+
+            window.overApps.startOverApp(options,function (success){
+                console.log(success);
+            },function (err){
+                console.log(err);
+            });
+        });
+
+    };
     return qmService;
 });
