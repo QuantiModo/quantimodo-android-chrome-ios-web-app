75d42bd2354b8c07110096bf943494129d9669f2
Mike P. Sinn <m@quantimodo.com>
2017-11-09 17:03:37 -0600

    qmLog.error("Refreshing user variables because we have more tracking reminders");

--- src/js/qmHelpers.js
+++ src/js/qmHelpers.js
@@ -147,6 +147,7 @@ window.qm = {
             var numberOfUserVariables = qm.userVariableHelper.getNumberOfUserVariablesInLocalStorage();
             qmLog.info(numberOfReminders + " reminders and " + numberOfUserVariables + " user variables in local storage");
             if(numberOfReminders > numberOfUserVariables){
+                qmLog.error("Refreshing user variables because we have more tracking reminders");
                 qm.userVariableHelper.refreshUserVariables();
             }
         },
