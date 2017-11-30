5c1bcd47e956d33e269691388ed7606be8b8c3a4
Mike P. Sinn <m@quantimodo.com>
2017-11-30 13:45:42 -0600

    Don't copy entire ion icons folder

--- gulpfile.js
+++ gulpfile.js
@@ -1868,7 +1868,7 @@ gulp.task('copyAppResources', [
     }).pipe(gulp.dest('.'));
 });
 gulp.task('copyIonIconsToWww', [], function () {
-    return copyFiles('src/lib/Ionicons/**/*', 'www/lib/Ionicons');
+    return copyFiles('src/lib/Ionicons/css/**/*', 'www/lib/Ionicons/css');
 });
 gulp.task('copyMaterialIconsToWww', [], function () {
     return copyFiles('src/lib/angular-material-icons/*', 'www/lib/angular-material-icons');
