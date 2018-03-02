a4dcd50527ca72d822199797f46dcad6d259b7d9
Mike P. Sinn <m@quantimodo.com>
2018-03-01 21:08:05 -0600

    timeout in updateNavigationMenuButton

--- gulpfile.js
+++ gulpfile.js
@@ -2053,7 +2053,7 @@ gulp.task('configureApp', [], function (callback) {
         'copySrcToWww',
         //'commentOrUncommentCordovaJs',
         'getCommonVariables',
-        //'getUnits',  // This is being weird for some reason
+        'getUnits',  // This is being weird for some reason
         'getAppConfigs',
         'minify-js-generate-css-and-index-html',
         'downloadIcon',
