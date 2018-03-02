57390dcbf8ce13c1a943ecef2a90f318cc8981c2
Mike P. Sinn <m@quantimodo.com>
2018-03-01 21:08:05 -0600

    timeout in updateNavigationMenuButton

--- src/js/controllers/predictorsCtrl.js
+++ src/js/controllers/predictorsCtrl.js
@@ -1,6 +1,6 @@
 angular.module('starter').controller('PredictorsCtrl', ["$scope", "$ionicLoading", "$state", "$stateParams", "qmService",
-    "qmLogService", "$rootScope", "$ionicActionSheet", "$mdDialog",
-    function($scope, $ionicLoading, $state, $stateParams, qmService, qmLogService, $rootScope, $ionicActionSheet, $mdDialog) {
+    "qmLogService", "$rootScope", "$ionicActionSheet", "$mdDialog", "$timeout",
+    function($scope, $ionicLoading, $state, $stateParams, qmService, qmLogService, $rootScope, $ionicActionSheet, $mdDialog, $timeout) {
     $scope.controller_name = "PredictorsCtrl";
     $scope.state = {
         variableName: null,
@@ -15,35 +15,40 @@ angular.module('starter').controller('PredictorsCtrl', ["$scope", "$ionicLoading
         $rootScope.showFilterBarSearchIcon = true;
         qmService.unHideNavigationMenu();
         if($stateParams.requestParams){ $scope.state.requestParams = $stateParams.requestParams; }
-        $rootScope.showActionSheetMenu = function() {
-            // Show the action sheet
-            var hideSheet = $ionicActionSheet.show({
-                buttons: [
-                    { text: '<i class="icon ion-arrow-down-c"></i>Descending Significance'},
-                    { text: '<i class="icon ion-arrow-down-c"></i>Descending QM Score' },
-                    { text: '<i class="icon ion-arrow-down-c"></i>Positive Relationships' },
-                    { text: '<i class="icon ion-arrow-up-c"></i>Negative Relationships' },
-                    { text: '<i class="icon ion-arrow-down-c"></i>Number of Participants' },
-                    { text: '<i class="icon ion-arrow-up-c"></i>Ascending pValue' },
-                    { text: '<i class="icon ion-arrow-down-c"></i>Optimal Pearson Product' },
-                    qmService.actionSheetButtons.refresh
-                ],
-                cancelText: '<i class="icon ion-ios-close"></i>Cancel',
-                cancel: function() { qmLogService.debug('CANCELLED', null); },
-                buttonClicked: function(index) {
-                    if(index === 0){populateCorrelationList('-statisticalSignificance');}
-                    if(index === 1){populateCorrelationList('-qmScore');}
-                    if(index === 2){populateCorrelationList('correlationCoefficient');}
-                    if(index === 3){populateCorrelationList('-correlationCoefficient');}
-                    if(index === 4){populateCorrelationList('-numberOfUsers');}
-                    if(index === 5){populateCorrelationList('pValue');}
-                    if(index === 6){populateCorrelationList('-optimalPearsonProduct');}
-                    if(index === 7){$scope.refreshList();}
-                    return true;
-                }
-            });
-        };
+        updateNavigationMenuButton();
     });
+    function updateNavigationMenuButton() {
+        $timeout(function() {
+            $rootScope.showActionSheetMenu = function() {
+                // Show the action sheet
+                var hideSheet = $ionicActionSheet.show({
+                    buttons: [
+                        { text: '<i class="icon ion-arrow-down-c"></i>Descending Significance'},
+                        { text: '<i class="icon ion-arrow-down-c"></i>Descending QM Score' },
+                        { text: '<i class="icon ion-arrow-down-c"></i>Positive Relationships' },
+                        { text: '<i class="icon ion-arrow-up-c"></i>Negative Relationships' },
+                        { text: '<i class="icon ion-arrow-down-c"></i>Number of Participants' },
+                        { text: '<i class="icon ion-arrow-up-c"></i>Ascending pValue' },
+                        { text: '<i class="icon ion-arrow-down-c"></i>Optimal Pearson Product' },
+                        qmService.actionSheetButtons.refresh
+                    ],
+                    cancelText: '<i class="icon ion-ios-close"></i>Cancel',
+                    cancel: function() { qmLogService.debug('CANCELLED', null); },
+                    buttonClicked: function(index) {
+                        if(index === 0){populateCorrelationList('-statisticalSignificance');}
+                        if(index === 1){populateCorrelationList('-qmScore');}
+                        if(index === 2){populateCorrelationList('correlationCoefficient');}
+                        if(index === 3){populateCorrelationList('-correlationCoefficient');}
+                        if(index === 4){populateCorrelationList('-numberOfUsers');}
+                        if(index === 5){populateCorrelationList('pValue');}
+                        if(index === 6){populateCorrelationList('-optimalPearsonProduct');}
+                        if(index === 7){$scope.refreshList();}
+                        return true;
+                    }
+                });
+            };
+        }, 1);
+    }
     // Have to get url params after entering.  Otherwise, we get params from study if coming back
     $scope.$on('$ionicView.afterEnter', function(e) {
         qm.loaders.robots();
