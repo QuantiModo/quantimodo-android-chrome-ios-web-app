29197f6334240b6dc42be4e72b61c239debd64ec
Mike P. Sinn <m@quantimodo.com>
2018-04-01 11:18:37 -0500

    chromeAndroidHelpers

--- src/js/services/qmService.js
+++ src/js/services/qmService.js
@@ -122,6 +122,77 @@ angular.module('starter').factory('qmService', ["$http", "$q", "$rootScope", "$i
             }
         },
         notifications: {
+            chromeAndroidHelpers: function(){
+                var start = Date.now();
+                var logsBuffer = '';
+                function log(msg) {
+                    msg = Date.now() - start + ': ' + msg;
+                    logsBuffer += msg + '\n';
+                    var logsDiv = document.getElementById('logs');
+                    if (logsDiv) {
+                        logsDiv.textContent = logsBuffer;
+                    }
+                    console.log(msg);
+                }
+                function initCallbacks() {
+                    chrome.alarms.onAlarm.addListener(function(alarm) {
+                        log("Received alarm: " + alarm.name + '. Creating notification.');
+                        createNotification('created from alarm');
+                    });
+                    chrome.notifications.onClosed.addListener(function(notificationId, byUser) {
+                        log('notifications.onClosed fired. id = ' + notificationId + ', byUser = ' + byUser);
+                    });
+                    chrome.notifications.onClicked.addListener(function(notificationId) {
+                        log('notifications.onClicked fired. id = ' + notificationId);
+                        chrome.notifications.clear(notificationId, function(wasCleared) {});
+                        renderUi('notification clicked');
+                        log('Showing window via cordova.backgroundapp.show().');
+                        cordova.backgroundapp.show();
+                    });
+                    log('App started: ' + new Date());
+                    document.addEventListener('deviceready', function() {
+                        log('deviceready. resumeType=' + cordova.backgroundapp.resumeType);
+                        initCallbacks();
+                        // Don't bother drawing UI when running in the background.
+                        if (cordova.backgroundapp.resumeType == 'launch') {
+                            renderUi('initial launch');
+                        } else { // resumeType == ''
+                            log('Running in the background!');
+                        }
+                    });
+                    document.addEventListener('resume', function() {
+                        log('resume event. resumeType=' + cordova.backgroundapp.resumeType);
+                        if (cordova.backgroundapp.resumeType == 'normal-launch') {
+                            renderUi('user launch when backgrounded');
+                        }
+                    });
+                    createNotification('created from button');
+                    createAlarm(5);
+                }
+                var numIds = 0;
+                function createNotification(message) {
+                    var opts = {
+                        message: message,
+                        title: 'BackgroundApp Example',
+                        type: 'basic',
+                        iconUrl: ''
+                    };
+                    chrome.notifications.create('id' + numIds++, opts, function(notificationId) {
+                    });
+                }
+                function createAlarm(seconds) {
+                    var expectedFireTime = Date.now() + seconds * 1000;
+                    chrome.alarms.create('id' + numIds++ , { when: expectedFireTime });
+                }
+                var isRendered = false;
+                function renderUi(reason) {
+                    if (!isRendered) {
+                        log('Rendering UI due to ' + reason);
+                        document.body.style.display = '';
+                        isRendered = true;
+                    }
+                }
+            },
             enableDrawOverAppsPopups: function () {
                 qm.notifications.setLastPopupTime(null);
                 qmService.storage.setItem(qm.items.drawOverAppsPopupEnabled, true);
@@ -135,6 +206,7 @@ angular.module('starter').factory('qmService', ["$http", "$q", "$rootScope", "$i
                         qmLogService.error("window.overApps is undefined!");
                     }
                     qmService.notifications.showAndroidPopupForMostRecentNotification();
+                    qmService.chromeAndroidHelpers();
                 });
             },
             showEnablePopupsConfirmation: function (ev) {
@@ -4584,7 +4656,7 @@ angular.module('starter').factory('qmService', ["$http", "$q", "$rootScope", "$i
             qmLogService.debug('Can only schedule notification on mobile or Chrome extension', null);
             return;
         }
-        qmLogService.info('scheduleSingleMostFrequentLocalNotification', null);
+        qmLogService.info('scheduleSingleMostFrequentLocalNotification');
         if($rootScope.user.combineNotifications === false){
             qmLogService.debug('scheduleSingleMostFrequentLocalNotification: $rootScope.user.combineNotifications === false so we shouldn\'t be calling this function', null);
             //return;
@@ -6703,7 +6775,6 @@ angular.module('starter').factory('qmService', ["$http", "$q", "$rootScope", "$i
             //qmService.humanConnect();
         }
     };
-
     qmService.getDevCredentials = function(){
         return $http.get('dev-credentials.json').success(function(response) {
             if(typeof response !== "string"){
