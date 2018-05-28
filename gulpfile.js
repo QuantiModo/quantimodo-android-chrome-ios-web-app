631d7a8e5e47bd706433954a3a579cb6e863a3e9
Mike P. Sinn <m@quantimodo.com>
2018-05-28 18:56:32 -0500

    qmGit

--- gulpfile.js
+++ gulpfile.js
@@ -77,72 +77,6 @@ var appIds = {
     'quantimodo': true,
     'medimodo': true
 };
-var qmGit = {
-    branchName: null,
-    isMaster: function () {
-        return qmGit.branchName === "master"
-    },
-    isDevelop: function () {
-        return qmGit.branchName === "develop"
-    },
-    isFeature: function () {
-        return qmGit.branchName.indexOf("feature") !== -1;
-    },
-    getCurrentGitCommitSha: function () {
-        if(process.env.SOURCE_VERSION){return process.env.SOURCE_VERSION;}
-        try {
-            return require('child_process').execSync('git rev-parse HEAD').toString().trim()
-        } catch (error) {
-            qmLog.info(error);
-        }
-    },
-    accessToken: process.env.GITHUB_ACCESS_TOKEN,
-    getCommitMessage(callback){
-        var commandForGit = 'git log -1 HEAD --pretty=format:%s';
-        execute(commandForGit, function (error, output) {
-            var commitMessage = output.trim();
-            qmLog.info("Commit: "+ commitMessage);
-            if(callback) {callback(commitMessage);}
-        });
-    },
-    outputCommitMessageAndBranch: function () {
-        qmGit.getCommitMessage(function (commitMessage) {
-            qmGit.setBranchName(function (branchName) {
-                qmLog.info("===== Building " + commitMessage + " on "+ branchName + " =====");
-            })
-        })
-    },
-    setBranchName: function (callback) {
-        function setBranch(branch, callback) {
-            qmGit.branchName = branch.replace('origin/', '');
-            qmLog.info('current git branch: ' + qmGit.branchName);
-            if (callback) {callback(qmGit.branchName);}
-        }
-        if (qmGit.getBranchEnv()){
-            setBranch(qmGit.getBranchEnv(), callback);
-            return;
-        }
-        try {
-            git.revParse({args: '--abbrev-ref HEAD'}, function (err, branch) {
-                if(err){qmLog.error(err); return;}
-                setBranch(branch, callback);
-            });
-        } catch (e) {
-            qmLog.info("Could not set branch name because " + e.message);
-        }
-    },
-    getBranchEnv: function () {
-        function getNameIfNotHead(envName) {
-            if(process.env[envName] && process.env[envName].indexOf("HEAD") === -1){return process.env[envName];}
-            return false;
-        }
-        if(getNameIfNotHead('CIRCLE_BRANCH')){return process.env.CIRCLE_BRANCH;}
-        if(getNameIfNotHead('BUDDYBUILD_BRANCH')){return process.env.BUDDYBUILD_BRANCH;}
-        if(getNameIfNotHead('TRAVIS_BRANCH')){return process.env.TRAVIS_BRANCH;}
-        if(getNameIfNotHead('GIT_BRANCH')){return process.env.GIT_BRANCH;}
-    }
-};
-qmGit.setBranchName();
 var paths = {
     apk: {
         combinedRelease: "platforms/android/build/outputs/apk/android-release.apk",
@@ -254,6 +188,72 @@ var qmLog = {
         throw message;
     }
 };
+var qmGit = {
+    branchName: null,
+    isMaster: function () {
+        return qmGit.branchName === "master"
+    },
+    isDevelop: function () {
+        return qmGit.branchName === "develop"
+    },
+    isFeature: function () {
+        return qmGit.branchName.indexOf("feature") !== -1;
+    },
+    getCurrentGitCommitSha: function () {
+        if(process.env.SOURCE_VERSION){return process.env.SOURCE_VERSION;}
+        try {
+            return require('child_process').execSync('git rev-parse HEAD').toString().trim()
+        } catch (error) {
+            qmLog.info(error);
+        }
+    },
+    accessToken: process.env.GITHUB_ACCESS_TOKEN,
+    getCommitMessage(callback){
+        var commandForGit = 'git log -1 HEAD --pretty=format:%s';
+        execute(commandForGit, function (error, output) {
+            var commitMessage = output.trim();
+            qmLog.info("Commit: "+ commitMessage);
+            if(callback) {callback(commitMessage);}
+        });
+    },
+    outputCommitMessageAndBranch: function () {
+        qmGit.getCommitMessage(function (commitMessage) {
+            qmGit.setBranchName(function (branchName) {
+                qmLog.info("===== Building " + commitMessage + " on "+ branchName + " =====");
+            })
+        })
+    },
+    setBranchName: function (callback) {
+        function setBranch(branch, callback) {
+            qmGit.branchName = branch.replace('origin/', '');
+            qmLog.info('current git branch: ' + qmGit.branchName);
+            if (callback) {callback(qmGit.branchName);}
+        }
+        if (qmGit.getBranchEnv()){
+            setBranch(qmGit.getBranchEnv(), callback);
+            return;
+        }
+        try {
+            git.revParse({args: '--abbrev-ref HEAD'}, function (err, branch) {
+                if(err){qmLog.error(err); return;}
+                setBranch(branch, callback);
+            });
+        } catch (e) {
+            qmLog.info("Could not set branch name because " + e.message);
+        }
+    },
+    getBranchEnv: function () {
+        function getNameIfNotHead(envName) {
+            if(process.env[envName] && process.env[envName].indexOf("HEAD") === -1){return process.env[envName];}
+            return false;
+        }
+        if(getNameIfNotHead('CIRCLE_BRANCH')){return process.env.CIRCLE_BRANCH;}
+        if(getNameIfNotHead('BUDDYBUILD_BRANCH')){return process.env.BUDDYBUILD_BRANCH;}
+        if(getNameIfNotHead('TRAVIS_BRANCH')){return process.env.TRAVIS_BRANCH;}
+        if(getNameIfNotHead('GIT_BRANCH')){return process.env.GIT_BRANCH;}
+    }
+};
+qmGit.setBranchName();
 var majorMinorVersionNumbers = '2.8.';
 if(argv.clientSecret){process.env.QUANTIMODO_CLIENT_SECRET = argv.clientSecret;}
 process.env.npm_package_licenseText = null; // Pollutes logs
