51f9d05dbd2a5675af7cdba243e385635317785e
Mike P. Sinn <m@quantimodo.com>
2018-05-28 20:23:12 -0500

    GI tests

--- gi_test_all.sh
+++ gi_test_all.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+set +x
 if [ -z "$START_URL" ]
   then
     START_URL=https://medimodo.herokuapp.com/
     echo "No START_URL specified so falling back to $START_URL"
     git push git@heroku.com:medimodo.git master -ff
+    echo "=== Check build progress at https://dashboard.heroku.com/apps/medimodo/activity ==="
 else
     echo "Using START_URL $START_URL"
 fi
@@ -20,10 +22,10 @@ set -x
 GI_API_KEY=f5b531ccd55da08abf35fadabd7b7b04f3d64312
 SUITE_ID=56f5b92519d90d942760ea96  # Ionic
 URL="https://api.ghostinspector.com/v1/suites/${SUITE_ID}/execute/?startUrl=${START_URL}&clientId=${CLIENT_ID}&apiKey=${GI_API_KEY}&commit="$(git rev-parse HEAD)
-echo "curl ${URL} > ghostinspector.json"
 curl "${URL}" > ghostinspector.json
 echo "=== Check progress at https://app.ghostinspector.com/suites/56f5b92519d90d942760ea96 ==="
 php ghostinspector_parser.php
+set -x
 echo "===== BRANCH: ${GIT_BRANCH} ====="
 echo "===== COMMIT: $COMMIT_MESSAGE ====="
 # curl "https://api.ghostinspector.com/v1/suites/56f5b92519d90d942760ea96/execute/?startUrl=https://utopia.quantimo.do:4470/ionic/Modo/src/#/&clientId=oauth_test_client&apiKey=f5b531ccd55da08abf35fadabd7b7b04f3d64312&commit=eaf513d9b35aaa0e16133a79eb71fcdd0456702e" > ghostinspector.json
