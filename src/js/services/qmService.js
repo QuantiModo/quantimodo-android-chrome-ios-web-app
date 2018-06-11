78fdd8cede14abd9dbb5c105c57b6a5800b1a81e
Mike P. Sinn <m@quantimodo.com>
2018-06-11 13:31:54 -0500

    Google scopes have to be a string, not array

--- src/js/services/qmService.js
+++ src/js/services/qmService.js
@@ -325,12 +325,15 @@ angular.module('starter').factory('qmService', ["$http", "$q", "$rootScope", "$i
             googleMobileConnect: function (connector, ev, additionalParams, successHandler, errorHandler) {
                 document.addEventListener('deviceready', deviceReady, false);
                 function deviceReady() {
-                    window.plugins.googleplus.login({
+                    var scopes = connector.scopes.join(" ");
+                    var params = {
                         'scopes': connector.scopes, // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
                         'webClientId': '1052648855194.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
                         'offline': true // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
-                    }, function (response) {
-                        qmLog.authDebug('window.plugins.googleplus.login response:' + JSON.stringify(response));
+                    };
+                    qmLog.authDebug("googleplus.login with params: "+JSON.stringify(params));
+                    window.plugins.googleplus.login(params, function (response) {
+                        qmLog.authDebug('googleplus.login response:' + JSON.stringify(response));
                         qmService.connectors.connectWithAuthCode(response.serverAuthCode, connector);
                         if(successHandler){successHandler(response);}
                     }, function (errorMessage) {
