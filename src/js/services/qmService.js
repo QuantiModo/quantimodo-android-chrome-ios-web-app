d04efeef4600c1e4af3c837b9b901b4a0abf8e55
Mike P. Sinn <m@quantimodo.com>
2018-06-11 13:34:48 -0500

    'scopes': scopes, // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.

--- src/js/services/qmService.js
+++ src/js/services/qmService.js
@@ -327,7 +327,7 @@ angular.module('starter').factory('qmService', ["$http", "$q", "$rootScope", "$i
                 function deviceReady() {
                     var scopes = connector.scopes.join(" ");
                     var params = {
-                        'scopes': connector.scopes, // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+                        'scopes': scopes, // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
                         'webClientId': '1052648855194.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
                         'offline': true // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
                     };
