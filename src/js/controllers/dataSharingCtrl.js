962a4567671183aeb9b1761ac953c342d7576fca
Mike P. Sinn <m@quantimodo.com>
2018-06-21 20:36:33 -0500

    Data sharing controller

---
+++ src/js/controllers/dataSharingCtrl.js
@@ -0,0 +1,17 @@
+angular.module("starter").controller("DataSharingCtrl", ["$scope", "$state", "qmService", "qmLogService", "$stateParams",
+    "$ionicHistory", "$rootScope", "$timeout", "$ionicLoading", "wikipediaFactory", "$ionicActionSheet", "clipboard", "$mdDialog",
+    function($scope, $state, qmService, qmLogService, $stateParams, $ionicHistory, $rootScope,
+                                      $timeout, $ionicLoading, wikipediaFactory, $ionicActionSheet, clipboard, $mdDialog) {
+
+    $scope.controller_name = "DataSharingCtrl";
+    $scope.$on("$ionicView.beforeEnter", function() {
+
+    });
+    $scope.$on("$ionicView.enter", function() {
+
+    });
+    $scope.$on("$ionicView.afterEnter", function() {
+
+    });
+
+}]);
