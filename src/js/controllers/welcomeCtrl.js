3b4cb4c967f477c63eada2a926c1dab4e6a6623e
Mike P. Sinn <m@quantimodo.com>
2018-07-04 23:39:12 -0500

    Handle 401 response

--- src/js/controllers/welcomeCtrl.js
+++ src/js/controllers/welcomeCtrl.js
@@ -15,12 +15,12 @@ angular.module('starter').controller('WelcomeCtrl', ["$scope", "$state", "$rootS
     $rootScope.sendDailyEmailReminder = true;
     $scope.saveIntervalAndGoToLogin = function(primaryOutcomeRatingFrequencyDescription){
         $scope.saveInterval(primaryOutcomeRatingFrequencyDescription);
-        qmService.login.sendToLogin();
+        qm.auth.sendToLogin();
     };
     $scope.skipInterval = function(){
         $scope.showIntervalCard = false;
         qmLogService.debug('skipInterval: Going to login state...', null);
-        qmService.login.sendToLogin();
+        qm.auth.sendToLogin();
     };
     $scope.saveInterval = function(primaryOutcomeRatingFrequencyDescription){
         if(primaryOutcomeRatingFrequencyDescription){$scope.primaryOutcomeRatingFrequencyDescription = primaryOutcomeRatingFrequencyDescription;}
