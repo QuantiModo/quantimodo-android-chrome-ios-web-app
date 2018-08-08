05602c78be227d2619d2e5a532ef097eca136077
Mike P. Sinn <m@quantimodo.com>
2018-08-08 11:47:39 -0500

    getMostRecentNotification function

--- src/js/controllers/chatCtrl.js
+++ src/js/controllers/chatCtrl.js
@@ -1,12 +1,19 @@
 angular.module('starter').controller('ChatCtrl', ["$state", "$scope", "$rootScope", "$http", "qmService", "$stateParams",
 	function( $state, $scope, $rootScope, $http, qmService, $stateParams) {
 	$scope.controller_name = "ChatCtrl";
-	$scope.state = {};
+	$scope.state = {
+		trackingReminderNotification: null
+	};
 	qmService.navBar.setFilterBarSearchIcon(false);
 	$scope.$on('$ionicView.beforeEnter', function(e) {
 		qmLog.debug('beforeEnter state ' + $state.current.name);
         if ($stateParams.hideNavigationMenu !== true){qmService.navBar.showNavigationMenuIfHideUrlParamNotSet();}
+
+
 	});
     $scope.$on('$ionicView.afterEnter', function(e) {qmService.hideLoader();});
-
+    function getMostRecentNotification() {
+        $scope.state.trackingReminderNotification = qm.notifications.getMostRecentNotification();
+        $scope.state.messages.push($scope.state.trackingReminderNotification.listCard)
+    }
 }]);
