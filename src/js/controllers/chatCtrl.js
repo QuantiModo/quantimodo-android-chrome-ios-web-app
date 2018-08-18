3c916c6a43b66f723d271e5d3d38ce29f4eb546e
Mike P. Sinn <m@quantimodo.com>
2018-08-17 23:31:16 -0500

    Card template

--- src/js/controllers/chatCtrl.js
+++ src/js/controllers/chatCtrl.js
@@ -1,6 +1,7 @@
 angular.module('starter').controller('ChatCtrl', ["$state", "$scope", "$rootScope", "$http", "qmService", "$stateParams", "$timeout", "$q",
 	function( $state, $scope, $rootScope, $http, qmService, $stateParams, $timeout, $q) {
 		$scope.controller_name = "ChatCtrl";
+        qmService.navBar.setFilterBarSearchIcon(false);
 		$scope.state = {
 			dialogFlow: false,
 			trackingReminderNotification: null,
@@ -15,10 +16,34 @@ angular.module('starter').controller('ChatCtrl', ["$state", "$scope", "$rootScop
                 }
             }
 		};
+        $scope.$on('$ionicView.beforeEnter', function(e) {
+            qmLog.debug('beforeEnter state ' + $state.current.name);
+            if ($stateParams.hideNavigationMenu !== true){qmService.navBar.showNavigationMenuIfHideUrlParamNotSet();}
+        });
+        $scope.$on('$ionicView.afterEnter', function(e) {qmService.hideLoader();
+            if(!qm.getUser()){
+                qmService.login.sendToLoginIfNecessaryAndComeBack();
+                return;
+            }
+
+        });
 		if($scope.state.visualizationType === 'rainbow'){$scope.state.bodyCss = "background: hsl(250,10%,10%); overflow: hidden;"}
         if($scope.state.visualizationType === 'siri'){$scope.state.bodyCss = "background: radial-gradient(farthest-side, #182158 0%, #030414 100%) no-repeat fixed 0 0; margin: 0;"}
         if($scope.state.visualizationType === 'equalizer'){$scope.state.bodyCss = "background-color:#333;"}
-        qmService.navBar.setFilterBarSearchIcon(false);
+        function notification() {
+            qm.notifications.getMostRecentNotification(function (notification) {
+                $scope.circlePage = qm.notifications.getCirclePage(notification);
+            });
+            $timeout(function () {
+                qm.speech.getMostRecentNotificationAndTalk();
+            }, 1);
+        }
+        function getQuestion() {
+		    var text =
+            if(qm.urlHelper.getParam('type') === 'question'){
+		        var question = qm.urlHelper.getParam('type')
+            }
+        }
         function blurAll(){
             var tmp = document.createElement("input");
             document.body.appendChild(tmp);
@@ -34,22 +59,7 @@ angular.module('starter').controller('ChatCtrl', ["$state", "$scope", "$rootScop
                 container[0].focus();
             }
         };
-        $scope.$on('$ionicView.beforeEnter', function(e) {
-            qmLog.debug('beforeEnter state ' + $state.current.name);
-            if ($stateParams.hideNavigationMenu !== true){qmService.navBar.showNavigationMenuIfHideUrlParamNotSet();}
-        });
-        $scope.$on('$ionicView.afterEnter', function(e) {qmService.hideLoader();
-            if(!qm.getUser()){
-                qmService.login.sendToLoginIfNecessaryAndComeBack();
-                return;
-            }
-            qm.notifications.getMostRecentNotification(function (notification) {
-                $scope.circlePage = qm.notifications.getCirclePage(notification);
-            });
-            $timeout(function () {
-                qm.speech.getMostRecentNotificationAndTalk();
-            }, 1);
-        });
+
         $scope.state.userReply = function(reply) {
             reply = reply || $scope.state.userInputString;
             if(reply){$scope.state.userInputString = reply;}
