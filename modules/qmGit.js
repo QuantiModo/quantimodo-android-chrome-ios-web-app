bbbbb5e540df99fe997f8b83cfff91bd5c874bee
mikepsinn <m@quantimodo.com>
2018-12-10 15:10:38 -0600

    Log if we can't get commit on Heroku

--- modules/qmGit.js
+++ modules/qmGit.js
@@ -1,5 +1,29 @@
 var qmLog = require('./qmLog');
 var git = require('gulp-git');
+function execute(command, callback, suppressErrors, lotsOfOutput) {
+    var exec = require('child_process').exec;
+    var spawn = require('child_process').spawn; // For commands with lots of output resulting in stdout maxBuffer exceeded error
+    qmLog.info('executing ' + command);
+    if(lotsOfOutput){
+        var args = command.split(" ");
+        var program = args.shift();
+        var ps = spawn(program, args);
+        ps.on('exit', function (code, signal) {
+            qmLog.info(command + ' exited with ' + 'code '+ code + ' and signal '+ signal);
+            if(callback){callback();}
+        });
+        ps.stdout.on('data', function (data) {qmLog.info(command + ' stdout: ' + data);});
+        ps.stderr.on('data', function (data) {qmLog.error(command + '  stderr: ' + data);});
+        ps.on('close', function (code) {if (code !== 0) {qmLog.error(command + ' process exited with code ' + code);}});
+    } else {
+        var my_child_process = exec(command, function (error, stdout, stderr) {
+            if (error !== null) {if (suppressErrors) {qmLog.info('ERROR: exec ' + error);} else {qmLog.error('ERROR: exec ' + error);}}
+            callback(error, stdout);
+        });
+        my_child_process.stdout.pipe(process.stdout);
+        my_child_process.stderr.pipe(process.stderr);
+    }
+}
 var qmGit = {
     branchName: null,
     isMaster: function () {
@@ -23,7 +47,12 @@ var qmGit = {
         }
     },
     accessToken: process.env.GITHUB_ACCESS_TOKEN,
-    getCommitMessage(callback){
+    getCommitMessage: function(callback){
+        if(process.env.BUILDPACK_LOG_FILE){
+            qmLog.info("Can't get commit on Heroku");
+            callback("Can't get commit on Heroku");
+            return;
+        }
         var commandForGit = 'git log -1 HEAD --pretty=format:%s';
         execute(commandForGit, function (error, output) {
             var commitMessage = output.trim();
