a9ffd05ef43e76fd0d01b6d5bd39ab9414e39e1b
Mike P. Sinn <m@quantimodo.com>
2019-10-11 09:43:33 -0500

    Login error message

--- cypress/integration/patient_registration_spec.js
+++ cypress/integration/patient_registration_spec.js
@@ -1,28 +1,35 @@
 var appHostName = process.env.APP_HOST_NAME || 'https://local.quantimo.do';
 var unixTime = Math.floor(Date.now() / 1000);
-var physicianOAuthUrl = appHostName+ '/api/v1/oauth/authorize?response_type=token&scope=readmeasurements&client_id=m-thinkbynumbers-org';
-var importUrl = appHostName+ '/import';
-var registerUrl = appHostName+ '/api/v2/auth/register';
-var usernameInput = '#username-group > input';
-var emailInput = '#email-group > input';
-var pw = '#password-group > input';
-var pwConfirm = '#password-confirm-group > input';
-var registerButton = '#submit-button-group > div > input.btn.btn-primary';
-var acceptButton = '#button-approve';
-var errorMessageSelector = '#error-messages > li';
+var urls = {
+    demo: {
+        importPage: "https://demo.quantimo.do/#/app/import"
+    },
+    import: appHostName+ '/import',
+    register: appHostName+ '/api/v2/auth/register',
+    physicianOAuth: appHostName+ '/api/v1/oauth/authorize?response_type=token&scope=readmeasurements&client_id=m-thinkbynumbers-org',
+};
+var selectors = {
+    usernameInput:'#username-group > input',
+    emailInput:'#email-group > input',
+    pw:'#password-group > input',
+    pwConfirm:'#password-confirm-group > input',
+    registerButton:'#submit-button-group > div > input.btn.btn-primary',
+    acceptButton:'#button-approve',
+    errorMessageSelector:'#error-messages > li',
+};
 var testUsername = 'testuser'+unixTime;
 var testEmail = 'testuser'+unixTime+'@gmail.com';
 function enterPasswordsAndClickRegister(){
-    cy.get(pw).type('testing123');
-    cy.get(pwConfirm).type('testing123');
-    cy.get(registerButton).click();
+    cy.get(selectors.pw).type('testing123');
+    cy.get(selectors.pwConfirm).type('testing123');
+    cy.get(selectors.registerButton).click();
 }
 function validRegistration(){
     changeTestUsernameAndEmail();
-    cy.get(usernameInput)
+    cy.get(selectors.usernameInput)
         .clear()
         .type(testUsername);
-    cy.get(emailInput)
+    cy.get(selectors.emailInput)
         .clear()
         .type(testEmail);
     enterPasswordsAndClickRegister();
@@ -44,31 +51,40 @@ function skipIntro(){
     cy.get('.slider > .slider-slides > .slider-slide:nth-child(1) > .button-bar > #skipButtonIntro').click();
 }
 describe('Auth Tests', function() {
-    it.only('Connect Withings', function() {
+    it.only('Check demo page', function() {
         cy.clearCookies();
-        cy.visit(importUrl+"?accessToken=demo");
+        cy.visit(urls.demo.importPage);
         try {
-            cy.get("#connect-withings-button").click();
+            cy.get("#connect-weather-button").click();
+        } catch (e) {
+            console.info(e.message);
+        }
+    });
+    it('Connect Canada Weather', function() {
+        cy.clearCookies();
+        cy.visit(urls.import+"?accessToken=demo");
+        try {
+            cy.get("#connect-weather-button").click();
         } catch (e) {
             console.info(e.message);
         }
     });
     it('Patient creates account and is sent to OAuth url', function() {
         cy.clearCookies();
-        cy.visit(physicianOAuthUrl);
+        cy.visit(urls.physicianOAuth);
         validRegistration();
-        cy.url().should('include', physicianOAuthUrl);
-        cy.get(acceptButton).click();
+        cy.url().should('include', urls.physicianOAuth);
+        cy.get(selectors.acceptButton).click();
         checkIntroWithAccessToken();
         skipIntro();
     });
     it('Tries to create account with existing username', function() {
         cy.clearCookies();
-        cy.visit(registerUrl);
-        cy.get(usernameInput).type('mike');
-        cy.get(emailInput).type(testEmail);
+        cy.visit(urls.register);
+        cy.get(selectors.usernameInput).type('mike');
+        cy.get(selectors.emailInput).type(testEmail);
         enterPasswordsAndClickRegister();
-        cy.contains(errorMessageSelector, 'The user login has already been taken.');
+        cy.contains(selectors.errorMessageSelector, 'The user login has already been taken.');
         validRegistration();
         checkIntroWithAccessToken();
     });
