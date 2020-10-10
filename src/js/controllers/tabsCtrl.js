774db7617a36b76ac9691ef5a4edfe38f59c7210
Mike P. Sinn <m@quantimodo.com>
2020-10-10 17:29:55 -0500

    Got rid of qmLogService

--- src/js/controllers/tabsCtrl.js
+++ src/js/controllers/tabsCtrl.js
@@ -1,6 +1,6 @@
 angular.module('starter').controller('TabsCtrl', ["$scope", "$state", function($scope, $state){
     $scope.$on('$ionicView.beforeEnter', function(e){
-        qmLogService.debug('Entering state ' + $state.current.name, null);
+        qmLog.debug('Entering state ' + $state.current.name, null);
         $scope.tabsTitle = "Tabs Title";
     });
 }]);
