05dcabc22ddd08b5252ad669d21f274234aeb336
Mike P. Sinn <m@quantimodo.com>
2020-10-13 22:34:36 -0500

    searchForMoodFromMagnifyingGlassIcon

--- cypress/integration/ionic_variables_spec.js
+++ cypress/integration/ionic_variables_spec.js
@@ -15,15 +15,14 @@ function verifyAndDeleteMeasurement(variableString){
 }
 /**
  * @param {string} variableName
- * @param timeout
+ * @param waitTime // Need to wait for users variables to populate or we just get common from staticData
  */
-function searchForMoodFromMagnifyingGlassIcon(variableName, timeout = 1000){
-    //cy.wait(1000)
+function searchForMoodFromMagnifyingGlassIcon(variableName, waitTime = 5000){
     cy.get('#menu-search-button').click({force: true})
     cy.get('md-autocomplete-wrap.md-whiteframe-z1 > input[type="search"]').click({force: true})
     cy.get('md-autocomplete-wrap.md-whiteframe-z1 > input[type="search"]').type(variableName, {force: true})
     cy.log('Wait for filtering')
-    cy.wait(timeout)
+    cy.wait(waitTime)
     cy.get('#variable-item-title > span', {timeout: 60000})
         .contains(variableName)
         .click({force: true})
@@ -52,7 +51,7 @@ describe('Variables', function(){
     it('Goes to charts page from the variable action sheet', function(){
         cy.loginWithAccessTokenIfNecessary('/#/app/reminders-inbox', true)
         let variableName = 'Overall Mood'
-        searchForMoodFromMagnifyingGlassIcon(variableName, 1500)
+        searchForMoodFromMagnifyingGlassIcon(variableName)
         cy.clickActionSheetButtonContaining('Charts')
         cy.url().should('contain', 'charts')
         cy.log('Chart is present and titled')
@@ -77,14 +76,14 @@ describe('Variables', function(){
     it('Records measurement from the variable action sheet', function(){
         cy.loginWithAccessTokenIfNecessary('/#/app/reminders-inbox', true)
         let variableName = 'Overall Mood'
-        searchForMoodFromMagnifyingGlassIcon(variableName, 1500)
+        searchForMoodFromMagnifyingGlassIcon(variableName)
         cy.clickActionSheetButtonContaining('Record Measurement')
         recordRatingMeasurement(3)
     })
     it('Goes to predictors page from the variable action sheet', function(){
         cy.loginWithAccessTokenIfNecessary('/#/app/reminders-inbox', true)
         let variableName = 'Overall Mood'
-        searchForMoodFromMagnifyingGlassIcon(variableName, 1000)
+        searchForMoodFromMagnifyingGlassIcon(variableName)
         cy.clickActionSheetButtonContaining('Predictors')
         cy.get('.item.item-avatar > p', {timeout: 90000}).should('contain', variableName)
     })
