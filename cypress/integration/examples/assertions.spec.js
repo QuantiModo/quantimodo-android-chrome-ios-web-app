54b485d175dbcbd284dd2b5c828338b5d793e10b
Mike P. Sinn <m@quantimodo.com>
2020-10-16 12:08:58 -0500

    Add es6 to eslintrc + cypress updates

--- cypress/integration/examples/assertions.spec.js
+++ cypress/integration/examples/assertions.spec.js
@@ -1,4 +1,4 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
 
 context('Assertions', () => {
   beforeEach(() => {
@@ -163,6 +163,15 @@ context('Assertions', () => {
       }
 
       assert.isObject(person, 'value is object')
+    })
+
+    it('retries the should callback until assertions pass', () => {
+      cy.get('#random-number')
+        .should(($div) => {
+          const n = parseFloat($div.text())
+
+          expect(n).to.be.gte(1).and.be.lte(10)
+        })
     })
   })
 })
