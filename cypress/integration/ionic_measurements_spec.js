6464d50587212b06c9fcc1cead91f83bb9b62ce8
Mike P. Sinn <m@quantimodo.com>
2020-12-05 19:59:55 -0600

    cy.get('#variable-name', {timeout: 5000}).contains(variableName)

--- cypress/integration/ionic_measurements_spec.js
+++ cypress/integration/ionic_measurements_spec.js
@@ -196,7 +196,7 @@ describe('Measurements', function () {
             cy.get('#variable-name').contains(variableName)
             recordRatingCheckHistory(newMoodValue, variableName, valence)
             cy.loginWithAccessTokenIfNecessary('/#/app/measurement-add?id=' + measurementId)
-            cy.get('#variable-name').contains(variableName)
+            cy.get('#variable-name', {timeout: 5000}).contains(variableName)
             cy.wait(1000)
             goToHistoryForVariable(variableName)
             cy.get("#hidden-measurement-id-0").then(($el) => {
