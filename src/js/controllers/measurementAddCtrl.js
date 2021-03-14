d95a43669e0d0a5209176e48b5394b5f3e8acf95
Mike P. Sinn <m@quantimodo.com>
2021-03-14 13:16:57 -0500

    processMeasurement

--- src/js/controllers/measurementAddCtrl.js
+++ src/js/controllers/measurementAddCtrl.js
@@ -134,6 +134,7 @@ angular.module('starter').controller('MeasurementAddCtrl', [
             delete m.startTimeEpoch;
             delete m.startTimeString;
             m.variableCategoryName = getVariableCategoryName();
+            qm.measurements.processMeasurement(m)
             return m;
         }
         function showToast() {
@@ -153,7 +154,9 @@ angular.module('starter').controller('MeasurementAddCtrl', [
                 JSON.stringify(m));
             showToast();
             // Measurement only - post measurement. This is for adding or editing
-            var backStateParams = {};
+            var backStateParams = {
+                measurement: m,
+            };
             qm.measurements.postMeasurement(m, function(){
                 if(unitChanged){
                     qmLog.error("Syncing reminders because unit changed");
