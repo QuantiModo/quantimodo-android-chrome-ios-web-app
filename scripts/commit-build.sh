44c077e1cdae656dbb9e1320fa8b7656f42b2fc2
mikepsinn <m@quantimodo.com>
2021-05-21 19:25:37 -0500

    commit-build.sh

---
+++ scripts/commit-build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+SCRIPT_FOLDER=`dirname ${SCRIPT_PATH}`
+echo "SCRIPT_FOLDER is $SCRIPT_FOLDER"
+cd ${SCRIPT_FOLDER}
+cd ..
+export IONIC_PATH="$PWD"
+echo "IONIC_PATH is $IONIC_PATH"
+BUILD_REPO=$IONIC_PATH/tmp/qm-web-build
+
+set -x
+set -e
+
+git config user.email "m@quantimodo.com"
+git config user.name "mikepsinn"
+
+set +x
+git clone https://${GITHUB_TOKEN}@github.com/mikepsinn/qm-web-build.git $BUILD_REPO || true
+set -x
+rm -rf $BUILD_REPO/docs/* &> /dev/null
+cp -R $IONIC_PATH/src/* $BUILD_REPO/docs
+cd $BUILD_REPO
+git add -A  &> /dev/null
+git commit -m "updated by travis build #$TRAVIS_BUILD_NUMBER"
+git push
+
