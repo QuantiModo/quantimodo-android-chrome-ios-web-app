8f104f0d8fdf8b5653276e8e91ff597f062634b2
mikepsinn <m@quantimodo.com>
2021-06-30 13:28:17 -0500

    Compiled

--- ts/qm.api.js
+++ ts/qm.api.js
@@ -1,8 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.makeApiRequest = void 0;
-var env = require("./env-helper");
-var qmLog = require("./qm.log");
+var env = __importStar(require("./env-helper"));
+var qmLog = __importStar(require("./qm.log"));
 function outputApiErrorResponse(err, options) {
     if (!err || !err.response) {
         qmLog.error("No err.response provided to outputApiErrorResponse!  err: ", err);
@@ -40,3 +59,4 @@ function makeApiRequest(options, successHandler) {
     });
 }
 exports.makeApiRequest = makeApiRequest;
+//# sourceMappingURL=qm.api.js.map
