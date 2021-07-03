16bbdaf859faea017a2472d7b707bc41401cd541
mikepsinn <m@quantimodo.com>
2021-07-02 22:43:38 -0500

    source "$SCRIPT_FOLDER"/no-root.sh

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# shellcheck disable=SC2086
+# shellcheck disable=SC2006
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=`dirname ${SCRIPT_PATH}`
+echo "SCRIPT_FOLDER is $SCRIPT_FOLDER" && cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && echo "IONIC_PATH is $IONIC_PATH"
+source "$SCRIPT_FOLDER"/no-root.sh
 set +x
 command -v nvm >/dev/null 2>&1 || {
     echo >&2 "nvm is required, but it's not installed.  Trying to install it now...";
@@ -7,6 +12,7 @@ command -v nvm >/dev/null 2>&1 || {
     echo "Installing nvm..."
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash;
     echo "Loading nvm command for shell access..."
+    # shellcheck disable=SC2155
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")";
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 }
