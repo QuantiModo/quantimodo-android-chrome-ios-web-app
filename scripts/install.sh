6007b7fa43fcbab4e2569bc2f526f781841adb30
mikepsinn <m@quantimodo.com>
2021-07-02 19:25:32 -0500

    sudo apt install -y curl

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,7 @@ echo "SCRIPT_FOLDER is $SCRIPT_FOLDER" && cd "${SCRIPT_FOLDER}" && cd .. && expo
 set -xe
 command -v nvm >/dev/null 2>&1 || {
     echo >&2 "nvm is required, but it's not installed.  Trying to install it now...";
+    sudo apt install -y curl
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash;
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")";
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
