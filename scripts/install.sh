eb20de2c8e966372a86c5e5f8f02d824a5bbe8eb
mikepsinn <m@quantimodo.com>
2021-07-02 19:24:28 -0500

    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,12 @@
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=`dirname ${SCRIPT_PATH}`
 echo "SCRIPT_FOLDER is $SCRIPT_FOLDER" && cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && echo "IONIC_PATH is $IONIC_PATH"
 set -xe
-command -v nvm >/dev/null 2>&1 || { echo >&2 "nvm is required, but it's not installed.  Trying to install it now..."; curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash && source ~/.profile; }
+command -v nvm >/dev/null 2>&1 || {
+    echo >&2 "nvm is required, but it's not installed.  Trying to install it now...";
+    curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash;
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")";
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+ }
 nvm install 10
 nvm use 10
 node -v
