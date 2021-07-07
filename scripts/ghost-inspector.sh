36800702d4491261c0b65587f519b861c4db2f3f
mikepsinn <m@quantimodo.com>
2021-07-06 20:29:46 -0500

    log_start.sh

---
+++ scripts/ghost-inspector.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=$(dirname "${SCRIPT_PATH}")
+echo "SCRIPT_FOLDER is $SCRIPT_FOLDER" && cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && echo "IONIC_PATH is $IONIC_PATH"
+# shellcheck source=./log_start.sh
+source "$IONIC_PATH"/scripts/log_start.sh
+set -xe
+npm run test:ghostinspector
+# shellcheck source=./log_end.sh
+source "$IONIC_PATH"/scripts/log_end.sh
--- scripts/test.sh
+++ scripts/test.sh
@@ -7,22 +7,17 @@ set -xe
 sudo bash "${SCRIPT_FOLDER}"/output_commit_message_and_env.sh
 # shellcheck source=./no-root.sh
 source "$SCRIPT_FOLDER"/no-root.sh
-
-set -e
-npm run types
-npm run test:mocha
+# shellcheck source=./mocha.sh
+source "$SCRIPT_FOLDER"/mocha.sh
 if [[ ${GIT_BRANCH} = "origin/develop" ]]; then bash "${SCRIPT_FOLDER}"/commit-build.sh && exit 0; fi
 if [[ ${GIT_BRANCH} != *"feature"* && ${GIT_BRANCH} != *"renovate"* ]]; then exit 0; fi
-git push git@heroku.com:medimodo.git HEAD:master -f;
+# shellcheck source=./heroku.sh
+source "${SCRIPT_FOLDER}"/heroku.sh
 # shellcheck source=./cypress_install.sh
 source "${SCRIPT_FOLDER}"/cypress_install.sh
-npm run cy:run
-if [[ ! -f success-file ]] ; then
-    echo 'success-file is not there, aborting.'
-    exit 1
-else
-	echo "success-file exists so running ghostinspector tests..."
-fi
-npm run test:ghostinspector
+# shellcheck source=./cypress_run.sh
+source "${SCRIPT_FOLDER}"/cypress_run.sh
+# shellcheck source=./ghost-inspector.sh
+source "${SCRIPT_FOLDER}"/ghost-inspector.sh
 # shellcheck source=./log_end.sh
 source "$IONIC_PATH"/scripts/log_end.sh
