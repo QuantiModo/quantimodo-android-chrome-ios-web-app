ab8f415027deb869798eea3610d542e1516cd6eb
mikepsinn <m@quantimodo.com>
2021-07-06 22:51:39 -0500

    source "$IONIC_PATH"/scripts/log_start.sh "${BASH_SOURCE[0]}"

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@
 PARENT_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=`dirname ${PARENT_SCRIPT_PATH}`
 echo "SCRIPT_FOLDER is $SCRIPT_FOLDER" && cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && echo "IONIC_PATH is $IONIC_PATH"
 # shellcheck source=./log_start.sh
-source $IONIC_PATH/scripts/log_start.sh
+source $IONIC_PATH/scripts/log_start.sh "${BASH_SOURCE[0]}"
 set -e
 set -x
 # shellcheck source=./no-root.sh
@@ -25,4 +25,4 @@ if [[ ${NODE_NAME} = "sonicmaster-ubuntu" ]];
 fi
 npm run configure:app
 # shellcheck source=./log_start.sh
-source $IONIC_PATH/scripts/log_end.sh
+source $IONIC_PATH/scripts/log_end.sh "${BASH_SOURCE[0]}"
--- scripts/log_end.sh
+++ scripts/log_end.sh
@@ -4,6 +4,7 @@ LOGGER_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${
 cd "${SCRIPTS_FOLDER}" || exit 1
 cd ..
 export REPO_BASE_WITH_SLASH="$PWD/"
+PARENT_SCRIPT_PATH=$1
 REPLACEMENT=""
 CALLER_SCRIPT_WITHOUT_REPO="${PARENT_SCRIPT_PATH/$REPO_BASE_WITH_SLASH/$REPLACEMENT}"
 echo "====================================="
