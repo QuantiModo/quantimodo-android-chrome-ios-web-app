14b9a64606a188de696726b2db62083bb5ba25ab
mikepsinn <m@quantimodo.com>
2021-07-07 21:33:50 -0500

    set -x

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -4,7 +4,6 @@ set -e
 PARENT_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=$(dirname "${PARENT_SCRIPT_PATH}")
 # shellcheck source=./log_start.sh
 cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && source "$IONIC_PATH"/scripts/log_start.sh "${BASH_SOURCE[0]}"
-set -xe
 # shellcheck source=./no-root.sh
 source "$SCRIPT_FOLDER"/no-root.sh
 git push git@heroku.com:medimodo.git HEAD:master -f;
