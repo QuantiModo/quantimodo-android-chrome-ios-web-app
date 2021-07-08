0e37c925f55d451f826f0280dc47a21bfc61a4d2
mikepsinn <m@quantimodo.com>
2021-07-07 20:32:00 -0500

    nvm_install.sh

--- scripts/nvm_install.sh
+++ scripts/nvm_install.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+set +x
+PARENT_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=$(dirname "${PARENT_SCRIPT_PATH}")
+echo "SCRIPT_FOLDER is $SCRIPT_FOLDER" && cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && echo "IONIC_PATH is $IONIC_PATH"
 
-sudo apt-get update
-sudo apt-get install build-essential libssl-dev
-curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
+set -x
+sudo chown -R "$USER" ~/.nvm || true
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+set +x
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 echo "You'll probably have to restart the script to use nvm"
+# shellcheck source=./log_start.sh
+source "$IONIC_PATH"/scripts/log_end.sh "${BASH_SOURCE[0]}"
