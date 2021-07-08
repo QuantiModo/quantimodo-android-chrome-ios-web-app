0d982330a3648d3c9be2db858c99a4ffcb83ddde
mikepsinn <m@quantimodo.com>
2021-07-07 21:32:49 -0500

    "$IONIC_PATH"/scripts/log_start.sh "${BASH_SOURCE[0]}"

--- scripts/nvm_install.sh
+++ scripts/nvm_install.sh
@@ -2,7 +2,7 @@
 set +x
 set -e
 PARENT_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=$(dirname "${PARENT_SCRIPT_PATH}")
-cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && source "$IONIC_PATH"/scripts/log_start.sh  "${BASH_SOURCE[0]}"
+cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && source "$IONIC_PATH"/scripts/log_start.sh "${BASH_SOURCE[0]}"
 set -x
 sudo chown -R "$USER" ~/.nvm || true
 sudo apt install -y curl
