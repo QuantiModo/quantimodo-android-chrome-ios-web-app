4f62fe9784ab4ca9dbce0ef4039e0f98195d3e48
mikepsinn <m@quantimodo.com>
2021-07-07 20:58:36 -0500

    nvm_install.sh

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -3,17 +3,14 @@
 # shellcheck disable=SC2006
 PARENT_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=`dirname ${PARENT_SCRIPT_PATH}`
 echo "SCRIPT_FOLDER is $SCRIPT_FOLDER" && cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && echo "IONIC_PATH is $IONIC_PATH"
+# shellcheck source=./no-root.sh
 source "$SCRIPT_FOLDER"/no-root.sh
 set +x
 sudo chown -R $USER ~/.nvm
 command -v nvm >/dev/null 2>&1 || {
     echo >&2 "nvm is required, but it's not installed.  Trying to install it now...";
-    echo "Installing curl..."
-    sudo apt install -y curl
-    echo "Installing nvm..."
-    curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash;
-    echo "Loading nvm command for shell access..."
-    # shellcheck disable=SC2155
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")";
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    # shellcheck source=./nvm_load.sh
+    source "$IONIC_PATH"/scripts/install.sh
 }
+# shellcheck source=./nvm_load.sh
+source "$IONIC_PATH"/scripts/nvm_load.sh
--- scripts/nvm_install.sh
+++ scripts/nvm_install.sh
@@ -2,14 +2,13 @@
 set +x
 PARENT_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=$(dirname "${PARENT_SCRIPT_PATH}")
 echo "SCRIPT_FOLDER is $SCRIPT_FOLDER" && cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && echo "IONIC_PATH is $IONIC_PATH"
-
 set -x
 sudo chown -R "$USER" ~/.nvm || true
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-set +x
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
+sudo apt install -y curl
+echo "Installing nvm..."
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash;
+# shellcheck source=./nvm_load.sh
+source "$IONIC_PATH"/scripts/nvm_load.sh
 echo "You'll probably have to restart the script to use nvm"
 # shellcheck source=./log_start.sh
 source "$IONIC_PATH"/scripts/log_end.sh "${BASH_SOURCE[0]}"
