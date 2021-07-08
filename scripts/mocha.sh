bfb3594508323c2c556a4ed668b08fc3a7f727b7
mikepsinn <m@quantimodo.com>
2021-07-07 23:13:05 -0500

    source "$IONIC_PATH"/scripts/log_start.sh "${BASH_SOURCE[0]}"

--- scripts/mocha.sh
+++ scripts/mocha.sh
@@ -2,9 +2,8 @@
 set +x
 set -e
 PARENT_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=$(dirname "${PARENT_SCRIPT_PATH}")
-cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && source "$IONIC_PATH"/scripts/log_start.sh "${BASH_SOURCE[0]}"
 # shellcheck source=./log_start.sh
-source "$IONIC_PATH"/scripts/log_start.sh "${BASH_SOURCE[0]}"
+cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && source "$IONIC_PATH"/scripts/log_start.sh "${BASH_SOURCE[0]}"
 set -x
 npm run types
 npm run test:mocha
