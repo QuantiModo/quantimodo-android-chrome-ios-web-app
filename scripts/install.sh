659b3298e05e30b4fc390eb88b3b95ea7c983f48
mikepsinn <m@quantimodo.com>
2021-07-07 21:26:19 -0500

    Script cleanup

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 # shellcheck disable=SC2006
 # shellcheck disable=SC2086
-PARENT_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=`dirname ${PARENT_SCRIPT_PATH}`
-echo "SCRIPT_FOLDER is $SCRIPT_FOLDER" && cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && echo "IONIC_PATH is $IONIC_PATH"
-# shellcheck source=./log_start.sh
-source $IONIC_PATH/scripts/log_start.sh "${BASH_SOURCE[0]}"
+set +x
 set -e
-set -x
+PARENT_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")" && SCRIPT_FOLDER=`dirname ${PARENT_SCRIPT_PATH}`
+# shellcheck source=./log_start.sh
+cd "${SCRIPT_FOLDER}" && cd .. && export IONIC_PATH="$PWD" && source "$IONIC_PATH"/scripts/log_start.sh  "${BASH_SOURCE[0]}"
 # shellcheck source=./no-root.sh
 source "$SCRIPT_FOLDER"/no-root.sh
-sudo chown -R $USER ~/.nvm || true
-set +x
-echo "nvm install 10..."
-nvm install 10
-echo "nvm use 10..."
-nvm use 10
-set -x
-node -v
+# shellcheck source=./nvm.sh
+source "$SCRIPT_FOLDER"/nvm.sh 10
 npm install
 if [[ ${NODE_NAME} = "sonicmaster-ubuntu" ]];
     then
@@ -25,4 +18,4 @@ if [[ ${NODE_NAME} = "sonicmaster-ubuntu" ]];
 fi
 npm run configure:app
 # shellcheck source=./log_start.sh
-source $IONIC_PATH/scripts/log_end.sh "${BASH_SOURCE[0]}"
+source "$IONIC_PATH"/scripts/log_end.sh "${BASH_SOURCE[0]}"
